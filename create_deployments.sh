68abe59ec58b25fe4add4bd9b4e28bc2d1edb4a0
peaky76 <robertjamespeacock@gmail.com>
2023-02-09 23:54:50 +0000

    Deployments: Change BHA flow to 1am Weds

--- create_deployments.sh
+++ create_deployments.sh
@@ -5,7 +5,7 @@ storage=github/handykapp-etl
 infra=docker-container/handykapp-etl
 
 mkdir deployments
-poetry run prefect deployment build ./flows/bha.py:bha_extractor -n $name -sb $storage -ib $infra -q default -o ./deployments/bha-deployment.yaml --cron "0 23 * * 2"
+poetry run prefect deployment build ./flows/bha.py:bha_extractor -n $name -sb $storage -ib $infra -q default -o ./deployments/bha-deployment.yaml --cron "0 1 * * 3"
 poetry run prefect deployment build ./flows/rapid_horseracing.py:rapid_horseracing_extractor -n $name -sb $storage -ib $infra -q default -o ./deployments/rapid-horseracing-deployment.yaml --cron "0 2 * * *"
 poetry run prefect deployment apply ./deployments/bha-deployment.yaml
 poetry run prefect deployment apply ./deployments/rapid-horseracing-deployment.yaml
