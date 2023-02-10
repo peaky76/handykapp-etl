e8e2c0f19610bf7e74017e237a9ae4beb14e6a1f
peaky76 <robertjamespeacock@gmail.com>
2023-02-10 01:07:49 +0000

    Helpers: Adapt read_json fn to read csvs too

--- flows/helpers.py
+++ flows/helpers.py
@@ -1,5 +1,6 @@
+import csv
+import json
 from digital_ocean_client import client
-from json import loads
 from requests import get
 
 BUCKET_NAME = 'peaky76'
@@ -22,9 +23,16 @@ def get_files(dirname, modified_after=None):
     return [key for file in files if '.' in (key := file.get('Key'))]
 
 
-def read_json(file):
-    obj = client.get_object(Bucket=BUCKET_NAME, Key=file)
-    return loads(obj['Body'].read())
+def read_file(file_path):
+    obj = client.get_object(Bucket=BUCKET_NAME, Key=file_path)
+    stream = obj['Body'].read().decode('utf-8')
+    format = file_path.split('.')[-1]
+    output = {
+        'csv': lambda x: [row for row in csv.reader(x.splitlines())],
+        'json': lambda x: json.loads(x)
+    }
+
+    return output[format](stream)
 
 
 def write_file(content, filename):
