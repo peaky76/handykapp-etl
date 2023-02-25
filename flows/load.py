8b9bf6c8882c33375bf6356b49d891ef6bfd1048
peaky76 <robertjamespeacock@gmail.com>
2023-02-25 15:58:57 +0000

    feat: add mongo_client, load fn to drop db

---
+++ flows/load.py
@@ -0,0 +1,17 @@
+# To allow running as a script
+from pathlib import Path
+import sys
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from flows.mongo_client import client
+from prefect import flow, task
+
+
+@flow
+def create_fresh_database():
+    client.drop_database("handykapp")
+
+
+if __name__ == "__main__":
+    create_fresh_database()
