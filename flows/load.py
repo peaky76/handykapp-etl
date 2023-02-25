b1e3be59783cb020e25f0c4f7f80a75b4addcc23
peaky76 <robertjamespeacock@gmail.com>
2023-02-25 17:55:21 +0000

    feat: add test data insert to load

--- flows/load.py
+++ flows/load.py
@@ -8,9 +8,23 @@ from clients import mongo_client as client
 from prefect import flow, task
 
 
+@task
+def drop_database():
+    client.drop_database("handykapp")
+
+
 @flow
 def create_fresh_database():
-    client.drop_database("handykapp")
+    frankel = {
+        "name": "Frankel",
+        "country": "GB",
+        "yob": 2008,
+    }
+
+    drop_database()
+    db = client.handykapp
+    collection = db.horses
+    id = collection.insert_one(frankel)
 
 
 if __name__ == "__main__":
