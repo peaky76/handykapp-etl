303c695e0516b8c9f9a215cb9432901120153da2
peaky76 <robertjamespeacock@gmail.com>
2023-02-25 13:16:33 +0000

    build: add black, mypy, coverage, make black changes

--- flows/digital_ocean_client.py
+++ flows/digital_ocean_client.py
@@ -3,8 +3,10 @@ from prefect.blocks.system import Secret
 
 
 session = boto3.session.Session()
-client = session.client('s3',
-                        region_name='ams3',
-                        endpoint_url='https://ams3.digitaloceanspaces.com',
-                        aws_access_key_id=Secret.load("spaces-key").get(),
-                        aws_secret_access_key=Secret.load("spaces-secret").get())
+client = session.client(
+    "s3",
+    region_name="ams3",
+    endpoint_url="https://ams3.digitaloceanspaces.com",
+    aws_access_key_id=Secret.load("spaces-key").get(),
+    aws_secret_access_key=Secret.load("spaces-secret").get(),
+)
