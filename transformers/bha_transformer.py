27485a71f1941d4820521035d063e4a7299eea4c
peaky76 <robertjamespeacock@gmail.com>
2023-02-26 00:08:37 +0000

    feat: add get_ratings_files

--- transformers/bha_transformer.py
+++ transformers/bha_transformer.py
@@ -3,10 +3,33 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-from flows.helpers import fetch_content, write_file, get_last_occurrence_of
+from flows.helpers import get_files, read_file
 from prefect import flow, task
+import pendulum
+import yaml
+
+
+with open("api_info.yml", "r") as f:
+    api_info = yaml.load(f, Loader=yaml.loader.SafeLoader)
+
+SOURCE = api_info["bha"]["space_dir"]
+
+
+@task(tags=["BHA"], task_run_name="get_ratings_files")
+def get_ratings_files(date=None):
+    files = [file for file in get_files(SOURCE) if "ratings" in file]
+    if date:
+        files = [
+            file for file in files if pendulum.parse(date).format("YYYYMMDD") in file
+        ]
+    return files
 
 
 @flow
 def bha_transformer():
-    pass
+    data = read_file(f"{SOURCE}bha_ratings_20230221.csv")
+    print(data)
+
+
+if __name__ == "__main__":
+    bha_transformer()
