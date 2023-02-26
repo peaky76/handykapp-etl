5c9637af3f0075219ba0aee909e2dee06801b184
peaky76 <robertjamespeacock@gmail.com>
2023-02-26 00:08:37 +0000

    feat: add get_ratings_files

---
+++ tests/transformers/test_bha_transformer.py
@@ -0,0 +1,22 @@
+from prefect.testing.utilities import prefect_test_harness
+from transformers.bha_transformer import bha_transformer, get_ratings_files
+
+
+def test_get_ratings_files_only_returns_ratings_files(mocker):
+    with prefect_test_harness():
+        mocker.patch("transformers.bha_transformer.get_files").return_value = [
+            "bha_ratings_20210101.csv",
+            "bha_rating_changes_20210101.csv",
+            "bha_perf_figs_20210101.csv",
+        ]
+        assert ["bha_ratings_20210101.csv"] == get_ratings_files.fn()
+
+
+def test_get_ratings_files_only_returns_files_for_specified_date(mocker):
+    with prefect_test_harness():
+        mocker.patch("transformers.bha_transformer.get_files").return_value = [
+            "bha_ratings_20210101.csv",
+            "bha_ratings_20210202.csv",
+            "bha_ratings_20210303.csv",
+        ]
+        assert ["bha_ratings_20210101.csv"] == get_ratings_files.fn("2021-01-01")
