26251f2ea01dae5d5df93c12b3ff10d99cf469da
peaky76 <robertjamespeacock@gmail.com>
2023-02-26 18:57:45 +0000

    refactor: move bha, rapid from flows to extractors

--- create_deployments.sh
+++ create_deployments.sh
@@ -5,8 +5,8 @@ storage=github/handykapp-etl
 infra=docker-container/handykapp-etl
 
 mkdir deployments
-poetry run prefect deployment build ./flows/bha.py:bha_extractor -n $name -sb $storage -ib $infra -q default -o ./deployments/bha-deployment.yaml --cron "0 1 * * 3"
-poetry run prefect deployment build ./flows/rapid_horseracing.py:rapid_horseracing_extractor -n $name -sb $storage -ib $infra -q default -o ./deployments/rapid-horseracing-deployment.yaml --cron "0 2 * * *"
+poetry run prefect deployment build ./extractors/bha_extractor.py:bha_extractor -n $name -sb $storage -ib $infra -q default -o ./deployments/bha-deployment.yaml --cron "0 1 * * 3"
+poetry run prefect deployment build ./extractors/rapid_horseracing_extractor.py:rapid_horseracing_extractor -n $name -sb $storage -ib $infra -q default -o ./deployments/rapid-horseracing-deployment.yaml --cron "0 2 * * *"
 poetry run prefect deployment build ./flows/load.py:create_fresh_database -n $name -sb $storage -ib $infra -q default -o ./deployments/load.yaml --cron "35 23 * * *"
 
 poetry run prefect deployment apply ./deployments/bha-deployment.yaml
