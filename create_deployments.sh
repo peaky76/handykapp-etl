79480f8dfcda2db08424ad77fd196515a939ec5f
peaky76 <robertjamespeacock@gmail.com>
2023-03-06 22:05:39 +0000

    build: add load deployment to script

--- create_deployments.sh
+++ create_deployments.sh
@@ -5,10 +5,10 @@ storage=github/handykapp-etl
 infra=docker-container/handykapp-etl
 
 mkdir deployments
-poetry run prefect deployment build ./extractors/bha_extractor.py:bha_extractor -n $name -sb $storage -ib $infra -q default -o ./deployments/bha-deployment.yaml --cron "0 1 * * 3"
-poetry run prefect deployment build ./extractors/rapid_horseracing_extractor.py:rapid_horseracing_extractor -n $name -sb $storage -ib $infra -q default -o ./deployments/rapid-horseracing-deployment.yaml --cron "0 2 * * *"
-poetry run prefect deployment build ./flows/load.py:create_fresh_database -n $name -sb $storage -ib $infra -q default -o ./deployments/load.yaml --cron "35 23 * * *"
+poetry run prefect deployment build ./src/extractors/bha_extractor.py:bha_extractor -n $name -sb $storage -ib $infra -q default -o ./deployments/bha-deployment.yaml --cron "0 1 * * 3"
+poetry run prefect deployment build ./src/extractors/rapid_horseracing_extractor.py:rapid_horseracing_extractor -n $name -sb $storage -ib $infra -q default -o ./deployments/rapid-horseracing-deployment.yaml --cron "0 2 * * *"
+poetry run prefect deployment build ./src/loaders/load.py:load_database_afresh -n $name -sb $storage -ib $infra -q default -o ./deployments/load-database-afresh-deployment.yaml --cron "0 4 * * *"
 
 poetry run prefect deployment apply ./deployments/bha-deployment.yaml
 poetry run prefect deployment apply ./deployments/rapid-horseracing-deployment.yaml
-poetry run prefect deployment apply ./deployments/load.yaml
+poetry run prefect deployment apply ./deployments/load-database-afresh-deployment.yaml
