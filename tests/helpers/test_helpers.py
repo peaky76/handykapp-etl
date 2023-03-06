69add6e0277d0d8fb7eadc16d1901bbc67a41ab1
peaky76 <robertjamespeacock@gmail.com>
2023-03-06 22:14:52 +0000

    refactor: restructure tests folder, fix: import ref in helpers

--- tests/helpers/test_helpers.py
+++ tests/helpers/test_helpers.py
@@ -1,5 +1,5 @@
 from pendulum import parse
-from src.helpers import get_last_occurrence_of
+from src.helpers.helpers import get_last_occurrence_of
 
 
 def test_get_last_occurrence_of_when_day_is_tomorrow(mocker):
