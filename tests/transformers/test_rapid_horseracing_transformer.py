9d23a2ce85ebeb7bc12e86816535f0a36dc1d8b5
peaky76 <robertjamespeacock@gmail.com>
2023-03-06 23:58:09 +0000

    feat: add validate_distance

--- tests/transformers/test_rapid_horseracing_transformer.py
+++ tests/transformers/test_rapid_horseracing_transformer.py
@@ -1,4 +1,4 @@
-from transformers.rapid_horseracing_transformer import validate_date
+from transformers.rapid_horseracing_transformer import validate_date, validate_distance
 
 
 def test_validate_date_fails_for_none():
@@ -11,3 +11,35 @@ def test_validate_date_passes_for_valid_date():
 
 def test_validate_date_fails_for_invalid_date():
     assert not validate_date("2020-01-32")
+
+
+def test_validate_distance_passes_for_miles_and_furlongs():
+    assert validate_distance("1m2f")
+
+
+def test_validate_distance_passes_for_miles_and_furlongs_with_space():
+    assert validate_distance("1m 2f")
+
+
+def test_validate_distance_passes_for_miles():
+    assert validate_distance("1m")
+
+
+def test_validate_distance_passes_for_furlongs():
+    assert validate_distance("6f")
+
+
+def test_validate_distance_fails_for_none():
+    assert not validate_distance(None)
+
+
+def test_validate_distance_fails_for_invalid_string():
+    assert not validate_distance("1m2f3f")
+
+
+def test_validate_distance_fails_for_furlongs_before_miles():
+    assert not validate_distance("6f1m")
+
+
+def test_validate_distance_fails_for_furlongs_over_eight():
+    assert not validate_distance("9f")
