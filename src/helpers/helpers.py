de3559c44d5e27b4d62a4936d52a14fac18e1750
peaky76 <robertjamespeacock@gmail.com>
2023-03-06 22:46:02 +0000

    feat: add basic rapid_horseracing_transformer

--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -1,7 +1,7 @@
 import csv
 import json
 import pendulum
-from src.clients import spaces_client as client
+from clients import spaces_client as client
 from requests import get
 
 BUCKET_NAME = "peaky76"
