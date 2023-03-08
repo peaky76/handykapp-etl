5b6baed7d38765fe508b0e5db2972605ab2e06fe
peaky76 <robertjamespeacock@gmail.com>
2023-03-08 20:57:16 +0000

    fix: test names

--- tests/transformers/test_parsers.py
+++ tests/transformers/test_parsers.py
@@ -1,15 +1,15 @@
 from transformers.parsers import parse_horse, parse_sex
 
 
-def test_parse_horse_returns_correct_dict_when_country_not_supplied():
+def test_parse_horse_returns_correct_tuple_when_country_not_supplied():
     assert ("DOBBIN", None) == parse_horse("DOBBIN")
 
 
-def test_parse_horse_returns_correct_dict_when_country_supplied_with_space():
+def test_parse_horse_returns_correct_tuple_when_country_supplied_with_space():
     assert ("DOBBIN", "IRE") == parse_horse("DOBBIN (IRE)")
 
 
-def test_parse_horse_returns_correct_dict_when_country_supplied_without_space():
+def test_parse_horse_returns_correct_tuple_when_country_supplied_without_space():
     assert ("DOBBIN", "IRE") == parse_horse("DOBBIN(IRE)")
 
 
