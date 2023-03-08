b8973af146b90b9086d753278cfae3198a5e2487
peaky76 <robertjamespeacock@gmail.com>
2023-03-08 20:38:05 +0000

    feat: add parse_sex fn to parsers

--- src/transformers/parsers.py
+++ src/transformers/parsers.py
@@ -2,3 +2,7 @@ def parse_horse(horse):
     split_string = horse.replace(")", "").split("(")
     name, country = split_string if len(split_string) == 2 else (horse, None)
     return (name.strip(), country)
+
+
+def parse_sex(sex):
+    return "M" if sex.upper() in ["GELDING", "COLT", "STALLION", "RIG"] else "F"
