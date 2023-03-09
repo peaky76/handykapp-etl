36ce7cd6963bdcb1d69f60f688700c5790b14016
peaky76 <robertjamespeacock@gmail.com>
2023-03-09 23:24:17 +0000

    refactor: extract constant for mocker patch in test_helpers

--- tests/helpers/test_helpers.py
+++ tests/helpers/test_helpers.py
@@ -1,27 +1,23 @@
 from pendulum import parse
 from src.helpers.helpers import get_last_occurrence_of, log_validation_problem
 
+PENDULUM_IMPORT = "src.helpers.helpers.pendulum"
+
 
 def test_get_last_occurrence_of_when_day_is_tomorrow(mocker):
-    mocker.patch("src.helpers.helpers.pendulum").now.return_value = parse(
-        "2023-02-13"
-    )  # A Monday
+    mocker.patch(PENDULUM_IMPORT).now.return_value = parse("2023-02-13")  # A Monday
     weekday_int = 2
     assert parse("2023-02-07").date() == get_last_occurrence_of(weekday_int)
 
 
 def test_get_last_occurrence_of_when_day_is_today(mocker):
-    mocker.patch("src.helpers.helpers.pendulum").now.return_value = parse(
-        "2023-02-14"
-    )  # A Tuesday
+    mocker.patch(PENDULUM_IMPORT).now.return_value = parse("2023-02-14")  # A Tuesday
     weekday_int = 2
     assert parse("2023-02-14").date() == get_last_occurrence_of(weekday_int)
 
 
 def test_get_last_occurrence_of_when_day_is_yesterday(mocker):
-    mocker.patch("src.helpers.helpers.pendulum").now.return_value = parse(
-        "2023-02-15"
-    )  # A Wednesday
+    mocker.patch(PENDULUM_IMPORT).now.return_value = parse("2023-02-15")  # A Wednesday
     weekday_int = 2
     assert parse("2023-02-14").date() == get_last_occurrence_of(weekday_int)
 
