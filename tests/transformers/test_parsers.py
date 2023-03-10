1b38c69040c4cf39594249a6f1258acb938210b4
peaky76 <robertjamespeacock@gmail.com>
2023-03-10 00:30:27 +0000

    feat: add parse obstacle

--- tests/transformers/test_parsers.py
+++ tests/transformers/test_parsers.py
@@ -2,6 +2,7 @@ import pendulum
 from transformers.parsers import (
     parse_going,
     parse_horse,
+    parse_obstacle,
     parse_sex,
     parse_weight,
     parse_yards,
@@ -50,6 +51,46 @@ def test_parse_horse_returns_correct_tuple_when_country_supplied_without_space()
 
 def test_parse_horse_returns_correct_tuple_when_country_supplied_as_a_default():
     assert ("DOBBIN", "IRE") == parse_horse("DOBBIN", "IRE")
+
+
+def test_parse_obstacle_returns_correct_value_for_none():
+    assert None == parse_obstacle("A RACE")
+
+
+def test_parse_obstacle_returns_correct_value_for_chase():
+    assert "CHASE" == parse_obstacle("A CHASE")
+
+
+def test_parse_obstacle_returns_correct_value_for_steeplechase():
+    assert "CHASE" == parse_obstacle("A STEEPLECHASE")
+
+
+def test_parse_obstacle_returns_correct_value_for_embedded_use_of_chase():
+    assert None == parse_obstacle("A PURCHASE")
+
+
+def test_parse_obstacle_returns_correct_value_for_hurdle():
+    assert "HURDLE" == parse_obstacle("A HURDLE")
+
+
+def test_parse_obstacle_returns_correct_value_for_cross_country():
+    assert "CROSS-COUNTRY" == parse_obstacle("A CROSS COUNTRY")
+
+
+def test_parse_obstacle_returns_correct_value_for_cross_country_chase():
+    assert "CROSS-COUNTRY" == parse_obstacle("A CROSS COUNTRY CHASE")
+
+
+def test_parse_obstacle_returns_correct_value_for_national_hunt_flat():
+    assert None == parse_obstacle("A NATIONAL HUNT FLAT")
+
+
+def test_parse_obstacle_returns_correct_value_for_flat_race():
+    assert None == parse_obstacle("A BIG STAKES RACE")
+
+
+def test_parse_obstacle_is_case_insensitive():
+    assert "CHASE" == parse_obstacle("a chase")
 
 
 def test_parse_sex_returns_correct_value_for_gelding():
