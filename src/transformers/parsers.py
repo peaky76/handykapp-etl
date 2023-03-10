d4cf011444eb392a1abbb567f15340d71daff08e
peaky76 <robertjamespeacock@gmail.com>
2023-03-10 00:30:27 +0000

    feat: add parse obstacle

--- src/transformers/parsers.py
+++ src/transformers/parsers.py
@@ -1,3 +1,4 @@
+import re
 import pendulum
 
 
@@ -16,6 +17,19 @@ def parse_horse(horse, default_country=None):
     split_string = horse.replace(")", "").split("(")
     name, country = split_string if len(split_string) == 2 else (horse, default_country)
     return (name.upper().strip(), country)
+
+
+def parse_obstacle(race_title):
+    obstacle_types = {
+        r"\bCROSS(-|\s)COUNTRY\b": "CROSS-COUNTRY",
+        r"\b(STEEPLE)?CHASE\b": "CHASE",
+        r"\bHURDLE\b": "HURDLE",
+    }
+
+    for regex, obstacle in obstacle_types.items():
+        if re.compile(regex).search(race_title.upper()):
+            return obstacle
+    return None
 
 
 def parse_sex(sex):
