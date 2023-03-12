856a268ecae6f01ae88b448004917767b6dadb34
peaky76 <robertjamespeacock@gmail.com>
2023-03-12 22:15:48 +0000

    test: add test for write file

--- tests/helpers/test_helpers.py
+++ tests/helpers/test_helpers.py
@@ -6,6 +6,7 @@ from src.helpers.helpers import (
     get_files,
     get_last_occurrence_of,
     log_validation_problem,
+    write_file,
 )
 
 PENDULUM_IMPORT = "src.helpers.helpers.pendulum"
@@ -49,6 +50,12 @@ def test_get_last_occurrence_of_when_day_is_tomorrow(mocker):
     mocker.patch(PENDULUM_IMPORT).now.return_value = parse("2023-02-13")  # A Monday
     weekday_int = 2
     assert parse("2023-02-07").date() == get_last_occurrence_of(weekday_int)
+
+
+def test_write_file(mocker):
+    client = mocker.patch("src.helpers.helpers.client")
+    write_file("foobar", "foo.csv")
+    assert client.put_object.called
 
 
 def test_get_last_occurrence_of_when_day_is_today(mocker):
