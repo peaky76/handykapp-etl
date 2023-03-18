fa080987fbcf8aa5db67947ebb8460fbf435d7f3
peaky76 <robertjamespeacock@gmail.com>
2023-03-18 20:04:48 +0000

    test: spec_database

--- tests/loaders/test_load.py
+++ tests/loaders/test_load.py
@@ -8,14 +8,55 @@ from loaders.load import (
     select_dams,
     select_sires,
     select_trainers,
+    spec_database,
 )
-from prefect.context import TaskRunContext
+from pymongo import ASCENDING as ASC
 
 
 def test_drop_database(mocker):
     client = mocker.patch("src.loaders.load.client")
     drop_database.fn()
     assert client.drop_database.called_once_with("racing")
+
+
+def test_spec_database_adds_horse_unique_index(mocker):
+    create_index = mocker.patch("pymongo.collection.Collection.create_index")
+    spec_database.fn()
+    assert create_index.called_once_with(
+        [("name", ASC), ("country", ASC), ("year", ASC)], unique=True
+    )
+
+
+def test_spec_database_adds_horse_name_index(mocker):
+    create_index = mocker.patch("pymongo.collection.Collection.create_index")
+    spec_database.fn()
+    assert create_index.called_once_with("name")
+
+
+def test_spec_database_adds_people_name_index(mocker):
+    create_index = mocker.patch("pymongo.collection.Collection.create_index")
+    spec_database.fn()
+    assert create_index.called_once_with("name", unique=True)
+
+
+def test_spec_database_adds_racecourse_index(mocker):
+    create_index = mocker.patch("pymongo.collection.Collection.create_index")
+    spec_database.fn()
+    assert create_index.called_once_with([("name", ASC), ("country", ASC)], unique=True)
+
+
+def test_spec_database_adds_races_index(mocker):
+    create_index = mocker.patch("pymongo.collection.Collection.create_index")
+    spec_database.fn()
+    assert create_index.called_once_with(
+        [("racecourse", ASC), ("datetime", ASC)], unique=True
+    )
+
+
+def test_spec_database_adds_races_result_horse_index(mocker):
+    create_index = mocker.patch("pymongo.collection.Collection.create_index")
+    spec_database.fn()
+    assert create_index.called_once_with("result.horse")
 
 
 def test_load_people(mocker):
