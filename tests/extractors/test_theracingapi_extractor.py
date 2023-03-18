f29105c1b16df7a08e88297bd70cb4bfde170427
peaky76 <robertjamespeacock@gmail.com>
2023-03-18 20:18:41 +0000

    test theracingapi_extractor.extract_racecards

--- tests/extractors/test_theracingapi_extractor.py
+++ tests/extractors/test_theracingapi_extractor.py
@@ -1,7 +1,9 @@
+import pendulum
 from src.extractors.theracingapi_extractor import (
     SOURCE,
     DESTINATION,
     extract_countries,
+    extract_racecards,
     get_headers,
 )
 
@@ -30,3 +32,18 @@ def test_extract_countries(mocker):
         "name": "foobaristan"
     }
     assert {"name": "foobaristan"} == extract_countries.fn()
+
+
+def test_extract_racecards_for_tomorrow_as_default(mocker):
+    write_file = mocker.patch("src.extractors.theracingapi_extractor.write_file")
+    mocker.patch("src.extractors.theracingapi_extractor.fetch_content").return_value = [
+        {}
+    ]
+    mocker.patch("src.extractors.theracingapi_extractor.DESTINATION", "dir/")
+    mocker.patch("pendulum.now").return_value = pendulum.parse("2020-01-01")
+
+    extract_racecards.fn()
+
+    expected_destination = "dir/racecards/theracingapi_racecards_20200102.json"
+    assert 1 == write_file.call_count
+    assert mocker.call([{}], expected_destination) == write_file.call_args
