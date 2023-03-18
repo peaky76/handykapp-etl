6a7d0e225898df54cecc3b8e6c37f41a118874b6
peaky76 <robertjamespeacock@gmail.com>
2023-03-18 20:07:39 +0000

    test: theracingapi_extractor.get_headers

--- tests/extractors/test_theracingapi_extractor.py
+++ tests/extractors/test_theracingapi_extractor.py
@@ -1,4 +1,4 @@
-from src.extractors.theracingapi_extractor import SOURCE, DESTINATION
+from src.extractors.theracingapi_extractor import SOURCE, DESTINATION, get_headers
 
 
 def test_theracingapi_source():
@@ -9,3 +9,12 @@ def test_theracingapi_source():
 def test_theracingapi_destination():
     expected = "handykapp/theracingapi/"
     assert expected == DESTINATION
+
+
+def test_get_headers(mocker):
+    mocker.patch(
+        "src.extractors.theracingapi_extractor.Secret.load"
+    ).return_value.get.return_value = "<key>"
+    headers = get_headers()
+    assert "the-racing-api1.p.rapidapi.com" == headers["x-rapidapi-host"]
+    assert "<key>" == headers["x-rapidapi-key"]
