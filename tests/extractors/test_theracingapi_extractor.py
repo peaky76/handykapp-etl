e63a07533e91b8b7257ea30055f373dd72168553
peaky76 <robertjamespeacock@gmail.com>
2023-03-18 20:10:31 +0000

    test: extract_countries

--- tests/extractors/test_theracingapi_extractor.py
+++ tests/extractors/test_theracingapi_extractor.py
@@ -1,4 +1,9 @@
-from src.extractors.theracingapi_extractor import SOURCE, DESTINATION, get_headers
+from src.extractors.theracingapi_extractor import (
+    SOURCE,
+    DESTINATION,
+    extract_countries,
+    get_headers,
+)
 
 
 def test_theracingapi_source():
@@ -18,3 +23,10 @@ def test_get_headers(mocker):
     headers = get_headers()
     assert "the-racing-api1.p.rapidapi.com" == headers["x-rapidapi-host"]
     assert "<key>" == headers["x-rapidapi-key"]
+
+
+def test_extract_countries(mocker):
+    mocker.patch("src.extractors.theracingapi_extractor.fetch_content").return_value = {
+        "name": "foobaristan"
+    }
+    assert {"name": "foobaristan"} == extract_countries.fn()
