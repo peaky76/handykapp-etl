ad315199110a848834ac68a594ebc08813a89570
peaky76 <robertjamespeacock@gmail.com>
2023-05-30 00:23:48 +0100

    feat: is_dist_going_method

--- tests/extractors/test_formdata_extractor.py
+++ tests/extractors/test_formdata_extractor.py
@@ -1,4 +1,20 @@
-from extractors.formdata_extractor import is_race_date
+from extractors.formdata_extractor import is_dist_going, is_race_date
+
+
+def test_is_dist_going_true_for_turf_going():
+    assert is_dist_going("5G")
+
+
+def test_is_dist_going_true_for_aw_going():
+    assert is_dist_going("5d")
+
+
+def test_is_dist_going_true_for_decimal_dist():
+    assert is_dist_going("9.1G")
+
+
+def test_is_dist_going_false_with_non_dist_going():
+    assert not is_dist_going("85")
 
 
 def test_is_race_date_true_when_single_digit_day():
@@ -9,5 +25,5 @@ def test_is_race_date_true_when_double_digit_day():
     assert is_race_date("20Apr23")
 
 
-def test_is_race_date_with_non_date_returns_false():
+def test_is_race_date_false_with_non_date():
     assert not is_race_date("JMitchell")
