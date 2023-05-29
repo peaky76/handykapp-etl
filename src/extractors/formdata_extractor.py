f9149e00c3fdb34495eb22d4e6ceda5fac107ef4
peaky76 <robertjamespeacock@gmail.com>
2023-05-30 00:23:48 +0100

    feat: is_dist_going_method

--- src/extractors/formdata_extractor.py
+++ src/extractors/formdata_extractor.py
@@ -8,6 +8,11 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 from prefect import flow, task
 
 
+def is_dist_going(string: str) -> str:
+    dist_going_regex = r"\d\.?\d?[H|F|M|G|D|S|V|f|g|d|s]"
+    return bool(re.match(dist_going_regex, string))
+
+
 def is_race_date(string: str) -> str:
     date_regex = r"\d{1,2}[A-Z][a-z]{2}\d{2}"
     return bool(re.match(date_regex, string))
