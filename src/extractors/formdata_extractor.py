acbe78348af3c3d39956e3c485d2578ab92580a1
peaky76 <robertjamespeacock@gmail.com>
2023-05-30 23:35:55 +0100

    refactor: remove extract horse and run methods

--- src/extractors/formdata_extractor.py
+++ src/extractors/formdata_extractor.py
@@ -33,22 +33,6 @@ Run = namedtuple(
 )
 
 
-def extract_run(string: str) -> str:
-    elements = string.split(" ")
-    next_date_index = elements.index(next(e for e in elements if is_race_date(e)))
-    next_dist_going_index = elements.index(
-        next(e for e in elements[next_date_index:] if is_dist_going(e))
-    )
-    return elements[next_date_index : next_dist_going_index + 2]
-
-
-def extract_title(words: list[str]):
-    for i, word in enumerate(words):
-        if "FORMDATA" in word:
-            return " ".join(words[i : i + 4])
-    return None
-
-
 def is_dist_going(string: str) -> str:
     dist_going_regex = r"\d\.?\d?[H|F|M|G|D|S|V|f|g|d|s]"
     return bool(re.match(dist_going_regex, string))
