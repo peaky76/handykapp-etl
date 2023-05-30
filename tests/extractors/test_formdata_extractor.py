7c2474c9c672c402be0948e2e881d9ed72d75a6a
peaky76 <robertjamespeacock@gmail.com>
2023-05-30 23:35:55 +0100

    refactor: remove extract horse and run methods

--- tests/extractors/test_formdata_extractor.py
+++ tests/extractors/test_formdata_extractor.py
@@ -1,63 +1,8 @@
 from extractors.formdata_extractor import (
-    extract_run,
-    extract_title,
     is_dist_going,
     is_horse,
     is_race_date,
 )
-
-
-def test_extract_run():
-    input = "9-6 BMcHugh 11 6.3 65 6.1G 78 12Oct22 H 7 Not 12 8-11 CMurtagh 10 4.6 67 5D 74 22Apr23 H"
-    expected = [
-        "12Oct22",
-        "H",
-        "7",
-        "Not",
-        "12",
-        "8-11",
-        "CMurtagh",
-        "10",
-        "4.6",
-        "67",
-        "5D",
-        "74",
-    ]
-    assert expected == extract_run(input)
-
-
-def test_extract_title_when_all_elements_present():
-    words = [
-        "FLAT FORMDATA",
-        "21 MAY 22 - 21 MAY 23",
-        "5",
-        "AAD",
-        "AADDEEY (IRE)",
-        "6",
-        "Archie Watson",
-    ]
-    assert "FLAT FORMDATA 21 MAY 22 - 21 MAY 23 5 AAD" == extract_title(words)
-
-
-def test_extract_title_when_not_title():
-    words = [
-        "Archie Watson",
-        "F2",
-        "£15462",
-        "4Jun22",
-        "H",
-        "39",
-        "Eps",
-        "11",
-        "9-4",
-        "t5HarryBurns",
-        "7",
-        "16.5",
-        "63",
-        "12G",
-        "87",
-    ]
-    assert None == extract_title(words)
 
 
 def test_is_dist_going_true_for_turf_going():
