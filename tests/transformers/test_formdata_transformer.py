cfbb25b2a5bf4b24a1719db0e48436bd0e413699
peaky76 <robertjamespeacock@gmail.com>
2023-08-17 22:33:13 +0100

    refactor: rename parse_middle_details -> extract_middle_details

--- tests/transformers/test_formdata_transformer.py
+++ tests/transformers/test_formdata_transformer.py
@@ -9,7 +9,7 @@ from transformers.formdata_transformer import (
     get_formdatas,
     is_horse,
     is_race_date,
-    parse_middle_details,
+    extract_middle_details,
 )
 
 FORMDATA_FETCH = "transformers.formdata_transformer.get_files"
@@ -42,6 +42,105 @@ def test_extract_dist_going_for_aw_going():
 
 def test_extract_dist_going_for_decimal_dist():
     assert (float("9.1"), "G") == extract_dist_going("9.1G")
+
+
+def test_extract_middle_details_when_jockey_and_single_digit_position():
+    expected = {
+        "headgear": None,
+        "allowance": 0,
+        "jockey": "JFanning",
+        "position": "3",
+    }
+
+    assert expected == extract_middle_details("JFanning3")
+
+
+def test_extract_middle_details_when_jockey_and_double_digit_position():
+    expected = {
+        "headgear": None,
+        "allowance": 0,
+        "jockey": "JFanning",
+        "position": "12",
+    }
+
+    assert expected == extract_middle_details("JFanning12")
+
+
+def test_extract_middle_details_when_headgear_jockey_position():
+    expected = {
+        "headgear": "t",
+        "allowance": 0,
+        "jockey": "JFanning",
+        "position": "12",
+    }
+
+    assert expected == extract_middle_details("tJFanning12")
+
+
+def test_extract_middle_details_when_allowance_jockey_position():
+    expected = {
+        "headgear": None,
+        "allowance": 3,
+        "jockey": "HDavies",
+        "position": "12",
+    }
+
+    assert expected == extract_middle_details("3HDavies12")
+
+
+def test_extract_middle_details_when_headgear_allowance_jockey_position():
+    expected = {
+        "headgear": "t",
+        "allowance": 3,
+        "jockey": "HDavies",
+        "position": "12",
+    }
+
+    assert expected == extract_middle_details("t3HDavies12")
+
+
+def test_extract_middle_details_when_position_equal():
+    expected = {
+        "headgear": "t",
+        "allowance": 3,
+        "jockey": "HDavies",
+        "position": "=12",
+    }
+
+    assert expected == extract_middle_details("t3HDavies=12")
+
+
+def test_extract_middle_details_when_position_reordered():
+    expected = {
+        "headgear": "t",
+        "allowance": 3,
+        "jockey": "HDavies",
+        "position": "2p3",
+    }
+
+    assert expected == extract_middle_details("t3HDavies2p3")
+
+
+def test_extract_middle_details_when_position_is_disaster():
+    expected = {
+        "headgear": "t",
+        "allowance": 3,
+        "jockey": "HDavies",
+        "position": "p",
+    }
+
+    assert expected == extract_middle_details("t3HDaviesp")
+
+
+def test_extract_middle_details_when_position_includes_disqulification():
+    expected = {
+        "headgear": "t",
+        "allowance": 3,
+        "jockey": "HDavies",
+        "position": "3d",
+    }
+
+    assert expected == extract_middle_details("t3HDavies3d")
 
 
 def test_extract_prize():
@@ -150,102 +249,3 @@ def test_is_race_date_true_when_double_digit_day():
 
 def test_is_race_date_false_with_non_date():
     assert not is_race_date("JMitchell")
-
-
-def test_parse_middle_details_when_jockey_and_single_digit_position():
-    expected = {
-        "headgear": None,
-        "allowance": 0,
-        "jockey": "JFanning",
-        "position": "3",
-    }
-
-    assert expected == parse_middle_details("JFanning3")
-
-
-def test_parse_middle_details_when_jockey_and_double_digit_position():
-    expected = {
-        "headgear": None,
-        "allowance": 0,
-        "jockey": "JFanning",
-        "position": "12",
-    }
-
-    assert expected == parse_middle_details("JFanning12")
-
-
-def test_parse_middle_details_when_headgear_jockey_position():
-    expected = {
-        "headgear": "t",
-        "allowance": 0,
-        "jockey": "JFanning",
-        "position": "12",
-    }
-
-    assert expected == parse_middle_details("tJFanning12")
-
-
-def test_parse_middle_details_when_allowance_jockey_position():
-    expected = {
-        "headgear": None,
-        "allowance": 3,
-        "jockey": "HDavies",
-        "position": "12",
-    }
-
-    assert expected == parse_middle_details("3HDavies12")
-
-
-def test_parse_middle_details_when_headgear_allowance_jockey_position():
-    expected = {
-        "headgear": "t",
-        "allowance": 3,
-        "jockey": "HDavies",
-        "position": "12",
-    }
-
-    assert expected == parse_middle_details("t3HDavies12")
-
-
-def test_parse_middle_details_when_position_equal():
-    expected = {
-        "headgear": "t",
-        "allowance": 3,
-        "jockey": "HDavies",
-        "position": "=12",
-    }
-
-    assert expected == parse_middle_details("t3HDavies=12")
-
-
-def test_parse_middle_details_when_position_reordered():
-    expected = {
-        "headgear": "t",
-        "allowance": 3,
-        "jockey": "HDavies",
-        "position": "2p3",
-    }
-
-    assert expected == parse_middle_details("t3HDavies2p3")
-
-
-def test_parse_middle_details_when_position_is_disaster():
-    expected = {
-        "headgear": "t",
-        "allowance": 3,
-        "jockey": "HDavies",
-        "position": "p",
-    }
-
-    assert expected == parse_middle_details("t3HDaviesp")
-
-
-def test_parse_middle_details_when_position_includes_disqulification():
-    expected = {
-        "headgear": "t",
-        "allowance": 3,
-        "jockey": "HDavies",
-        "position": "3d",
-    }
-
-    assert expected == parse_middle_details("t3HDavies3d")
