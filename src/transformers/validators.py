e2bbe9f79ead859427fc61d109dbf377806bc376
peaky76 <robertjamespeacock@gmail.com>
2023-09-10 10:31:11 +0100

    refactor: use Horsetalk going descriptions in going validator

--- src/transformers/validators.py
+++ src/transformers/validators.py
@@ -1,5 +1,6 @@
-import re
 import pendulum
+import re
+from horsetalk import AWGoingDescription, TurfGoingDescription
 
 
 def validate_class(race_class):
@@ -25,22 +26,7 @@ def validate_going(going):
     if not going:
         return False
 
-    goings = [
-        "HARD",
-        "FIRM",
-        "GOOD TO FIRM",
-        "GOOD",
-        "GOOD TO SOFT",
-        "SOFT",
-        "SOFT TO HEAVY",
-        "HEAVY",
-        "STANDARD",
-        "STANDARD TO SLOW",
-        "SLOW",
-        "GOOD TO YIELDING",
-        "YIELDING",
-        "YIELDING TO SOFT",
-    ]
+    goings = [g.name for d in [TurfGoingDescription, AWGoingDescription] for g in d]
     going = going.upper().replace(")", "").replace(" IN PLACES", "").split(" (")
     return any(g in going[0] for g in goings) and (
         any(g in going[1] for g in goings) if len(going) == 2 else True
