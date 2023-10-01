02958688e946ee645f7cfd0d05892d8ec1b99b89
peaky76 <robertjamespeacock@gmail.com>
2023-10-01 13:50:12 +0100

    refactor: move db drop and spec to separate file, rename load -> bha_loader

--- src/loaders/bha_loader.py
+++ src/loaders/bha_loader.py
@@ -4,41 +4,18 @@ import sys
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-from clients import mongo_client as client
 from transformers.bha_transformer import bha_transformer
 from transformers.parsers import parse_horse
-from transformers.core_transformer import core_transformer
-from loaders.adders import add_horse, add_person, add_racecourse
+from loaders.adders import add_horse, add_person
 from nameparser import HumanName  # type: ignore
 from prefect import flow, task, get_run_logger
-from pymongo import ASCENDING as ASC
+
 import yaml
 
 with open("api_info.yml", "r") as f:
     api_info = yaml.load(f, Loader=yaml.loader.SafeLoader)
 
 SOURCE = api_info["bha"]["spaces"]["dir"]
-
-db = client.handykapp
-
-
-@task
-def drop_database():
-    client.drop_database("handykapp")
-
-
-@task
-def spec_database():
-    db.horses.create_index(
-        [("name", ASC), ("country", ASC), ("year", ASC)], unique=True
-    )
-    db.horses.create_index("name")
-    db.people.create_index(
-        [("last", ASC), ("first", ASC), ("middle", ASC)], unique=True
-    )
-    db.racecourses.create_index([("name", ASC), ("country", ASC)], unique=True)
-    db.races.create_index([("venue", ASC), ("datetime", ASC)], unique=True)
-    db.races.create_index("result.horse")
 
 
 def convert_parent(name, sex):
@@ -84,34 +61,16 @@ def load_people(people, source):
     return ret_val
 
 
-@task(tags=["Rapid"])
-def load_races(races):
-    for race in races:
-        del race["result"]
-        db.races.insert_one(race)
-
-
-@task
-def load_ratings():
-    pass  # TODO
+# @task(tags=["Rapid"])
+# def load_races(races):
+#     for race in races:
+#         del race["result"]
+#         db.races.insert_one(race)
 
 
 @task(tags=["BHA"])
 def select_set(data, key):
     return sorted(list(set([x[key] for x in data])))
-
-
-@flow
-def create_sample_database():
-    frankel = {
-        "name": "Frankel",
-        "country": "GB",
-        "yob": 2008,
-    }
-
-    drop_database()
-    spec_database()
-    db.horses.insert_one(frankel)
 
 
 @flow
@@ -130,7 +89,7 @@ def load_bha_horses():
 
 
 @flow
-def load_database_afresh():
+def load_bha_afresh():
     pass
     # drop_database()
     # spec_database()
