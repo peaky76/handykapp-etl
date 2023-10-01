18ed227203ca707315caa29902fb4230a1d16163
peaky76 <robertjamespeacock@gmail.com>
2023-10-01 22:45:39 +0100

    refactor: move convert_person to shared

--- tests/loaders/test_shared.py
+++ tests/loaders/test_shared.py
@@ -1,4 +1,17 @@
-from loaders.shared import select_set
+from loaders.shared import convert_person, select_set
+
+
+def test_convert_person():
+    expected = {
+        "title": "Mr.",
+        "first": "John",
+        "middle": "",
+        "last": "Smith",
+        "suffix": "",
+        "nickname": "",
+        "display_name": {"BHA": "Mr. John Smith"},
+    }
+    assert expected == convert_person("Mr. John Smith", "BHA")
 
 
 def test_select_set():
