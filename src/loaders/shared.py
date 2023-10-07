d316ba836384d5e7d8363b829223f9330f98e939
peaky76 <robertjamespeacock@gmail.com>
2023-10-07 21:25:29 +0100

    refactor: move convert_value_to_id to shared

--- src/loaders/shared.py
+++ src/loaders/shared.py
@@ -1,11 +1,21 @@
-import re
-from nameparser import HumanName  # type: ignore
+from nameparser import HumanName
+from transformers.parsers import parse_horse  # type: ignore
 
 
 def convert_person(name, source):
     parsed_name = HumanName(name).as_dict()
     parsed_name["display_name"] = {source: name}
     return parsed_name
+
+
+def convert_value_to_id(horse, value, lookup):
+    if value:
+        horse[value] = (
+            lookup.get(parse_horse(horse[value]), None)
+            if value in ["sire", "dam"]
+            else lookup.get(horse[value], None)
+        )
+    return horse
 
 
 def select_set(data, key):
