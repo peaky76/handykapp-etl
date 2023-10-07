31ba222f8c29c34d8c37c13b3fde6db57e4c7ed3
peaky76 <robertjamespeacock@gmail.com>
2023-10-07 23:21:19 +0100

    refactor: more typing on MongoRace

--- src/models/mongo_race.py
+++ src/models/mongo_race.py
@@ -3,6 +3,21 @@ from pendulum import DateTime
 from typing import TypedDict
 
 
+class MongoRaceRestriction(TypedDict):
+    minimum: int | None
+    maximum: int | None
+
+
 class MongoRace(TypedDict, total=False):
     racecourse: ObjectId
     datetime: DateTime
+    title: str
+    is_handicap: bool
+    obstacle: str | None
+    distance_description: str
+    race_grade: str
+    race_class: int
+    age_restriction: MongoRaceRestriction
+    rating_restriction: MongoRaceRestriction
+    going_description: str
+    prize: str
