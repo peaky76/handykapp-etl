c0f5ea3246b8d3efe453948aaa019092f04cd137
peaky76 <robertjamespeacock@gmail.com>
2024-01-19 21:23:58 +0000

    refactor: rename dicts as record_processor, handle single dict

--- src/loaders/rapid_horseracing_loader.py
+++ src/loaders/rapid_horseracing_loader.py
@@ -14,7 +14,7 @@ from transformers.rapid_horseracing_transformer import (
     validate_results,
 )
 
-from loaders.dicts_processor import dicts_processor
+from loaders.record_processor import record_processor
 
 with open("settings.toml", "rb") as f:
     settings = tomllib.load(f)
@@ -29,17 +29,17 @@ def load_rapid_horseracing_data():
     logger = get_run_logger()
     logger.info("Starting rapid_horseracing loader")
 
-    d = dicts_processor()
-    next(d)
+    r = record_processor()
+    next(r)
 
     files = get_files(f"{SOURCE}results")
 
     for file in files:
         if file != "results_to_do_list.json":
-            contents = read_file(file)
-            d.send(([contents], validate_results, transform_results, file, "rapid"))
+            record = read_file(file)
+            r.send((record, validate_results, transform_results, file, "rapid"))
 
-    d.close()
+    r.close()
 
 
 if __name__ == "__main__":
--- src/loaders/theracingapi_loader.py
+++ src/loaders/theracingapi_loader.py
@@ -11,7 +11,7 @@ from helpers import get_files, read_file
 from prefect import flow, get_run_logger
 from transformers.theracingapi_transformer import transform_races, validate_races
 
-from loaders.dicts_processor import dicts_processor
+from loaders.record_processor import record_processor
 
 with open("settings.toml", "rb") as f:
     settings = tomllib.load(f)
@@ -26,17 +26,15 @@ def load_theracingapi_data():
     logger = get_run_logger()
     logger.info("Starting theracingapi loader")
 
-    d = dicts_processor()
-    next(d)
+    r = record_processor()
+    next(r)
     for file in get_files(f"{SOURCE}racecards"):
         contents = read_file(file)
-        filtered_contents = [
-            {k: v for k, v in dec.items() if k != "off_dt"}
-            for dec in contents["racecards"]
-        ]
-        d.send((filtered_contents, validate_races, transform_races, file, "theracingapi"))
+        for dec in contents["racecards"]:
+            record =  {k: v for k, v in dec.items() if k != "off_dt"}
+            r.send((record, validate_races, transform_races, file, "theracingapi"))
 
-    d.close()
+    r.close()
 
 
 if __name__ == "__main__":
