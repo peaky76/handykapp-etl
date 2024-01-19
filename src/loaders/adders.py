9ecffee8ab35eef50438bfdbd6c160c0d0b86bfc
peaky76 <robertjamespeacock@gmail.com>
2024-01-19 23:04:15 +0000

    refactor: inline insert racecourse

--- src/loaders/adders.py
+++ src/loaders/adders.py
@@ -18,7 +18,3 @@ def add_person(person):
     person_id = db.people.insert_one(person)
     return person_id.inserted_id
 
-
-def add_racecourse(racecourse):
-    racecourse_id = db.racecourses.insert_one(racecourse)
-    return racecourse_id.inserted_id
