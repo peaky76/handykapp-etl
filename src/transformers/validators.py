071585ac206922f273ca64bc9e3532321245b678
peaky76 <robertjamespeacock@gmail.com>
2024-01-19 16:31:19 +0000

    refactor: going names in validate_going

--- src/transformers/validators.py
+++ src/transformers/validators.py
@@ -32,9 +32,7 @@ def validate_going(going):
     if not going:
         return False
 
-    goings = [g.name for d in [TurfGoingDescription, AWGoingDescription] for g in d] + [
-        "YIELDING"
-    ]
+    goings = [g for d in [TurfGoingDescription, AWGoingDescription] for g in d.__members__ if len(g) > 3]
     going = going.upper().replace(")", "").replace(" IN PLACES", "").split(" (")
     return any(g in going[0] for g in goings) and (
         any(g in going[1] for g in goings) if len(going) == 2 else True
