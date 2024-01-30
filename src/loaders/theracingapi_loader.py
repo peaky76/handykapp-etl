56b227ec9ff7dc179bf62f3145a04db5a0671c35
peaky76 <robertjamespeacock@gmail.com>
2024-01-30 22:40:53 +0000

    refactor: separate fn for incrementing theracingapi

--- src/loaders/theracingapi_loader.py
+++ src/loaders/theracingapi_loader.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-
+import pendulum
 import tomllib
 from clients import mongo_client as client
 from helpers import get_files, read_file
@@ -19,15 +19,25 @@ SOURCE = settings["theracingapi"]["spaces_dir"]
 
 db = client.handykapp
 
+@flow
+def increment_theracingapi_data():
+    most_recent = next(db.races.find().sort("datetime", -1))["datetime"]
+    load_theracingapi_data(from_date=pendulum.parse(most_recent))
 
 @flow
-def load_theracingapi_data():
+def load_theracingapi_data(*, from_date=None):
     logger = get_run_logger()
     logger.info("Starting theracingapi loader")
 
     r = record_processor()
     next(r)
     for file in get_files(f"{SOURCE}racecards"):
+        if from_date:
+            file_date = pendulum.parse(file.split(".")[0][-8:])
+            if file_date < from_date:
+                continue
+        
+        logger.info(f"Reading {file}")
         contents = read_file(file)
         for dec in contents["racecards"]:
             record = {k: v for k, v in dec.items() if k != "off_dt"}
@@ -37,4 +47,4 @@ def load_theracingapi_data():
 
 
 if __name__ == "__main__":
-    load_theracingapi_data()
+    increment_theracingapi_data()
