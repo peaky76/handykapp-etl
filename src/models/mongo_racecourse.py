43031164ad1d602a6f92bdf4751cfcd9d759f8a9
peaky76 <robertjamespeacock@gmail.com>
2024-02-26 22:25:59 +0000

    refactor: extra typing on MongoRacecourse

--- src/models/mongo_racecourse.py
+++ src/models/mongo_racecourse.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Literal, Optional
 
 from pydantic import BaseModel, Field
 
@@ -6,15 +6,23 @@ from models.mongo_references import MongoReferences
 
 from .pyobjectid import PyObjectId
 
+CodeType = Literal["Flat", "NH"]
+SurfaceType = Literal["Turf", "Dirt", "Fibresand", "Polytrack", "Tapeta"]
+ObstacleType = Literal["Hurdle", "Chase"]
+ShapeType = Literal["Circle", "Horseshoe", "Oval", "Pear", "Triangle"]
+Handedness = Literal["Left", "Right", "Both"]
+StyleType = Literal["Galloping", "Stiff", "Tight"]
+ContourType = Literal["Flat", "Undulating", "Uphill"]
 
 class MongoRacecourse(BaseModel):
     id: Optional[PyObjectId] = Field(alias="_id", default=None)
     name: str
     formal_name: Optional[str] = None
-    surface: Optional[str] = None
-    obstacle: str | None
-    shape: Optional[str] = None
-    handedness: Optional[str] = None
-    style: Optional[str] = None
-    contour: Optional[str] = None
+    code: Optional[CodeType] = None
+    surface: Optional[SurfaceType] = None
+    obstacle: ObstacleType | None
+    shape: Optional[ShapeType] = None
+    handedness: Optional[Handedness] = None
+    style: Optional[StyleType] = None
+    contour: Optional[ContourType] = None
     references: Optional[MongoReferences] = None
