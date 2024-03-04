9711377cb42ceb95d8d99957373b3ff8c16864ae
peaky76 <robertjamespeacock@gmail.com>
2024-03-04 20:34:28 +0000

    fix: models import/export

---
+++ src/models/__init__.py
@@ -0,0 +1,15 @@
+from .mongo_horse import MongoHorse
+from .mongo_person import MongoPerson
+from .mongo_race import MongoRace
+from .mongo_racecourse import MongoRacecourse
+from .mongo_references import MongoReferences
+from .py_object_id import PyObjectId
+
+__all__ = [
+    "MongoHorse",
+    "MongoPerson",
+    "MongoRace",
+    "MongoRacecourse",
+    "MongoReferences",
+    "PyObjectId",
+]
