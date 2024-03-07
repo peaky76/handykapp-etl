5be321c0092b85cfa622bbee7c90ca0891884bde
peaky76 <robertjamespeacock@gmail.com>
2024-03-07 23:16:23 +0000

    feat: ProcessPerson type

---
+++ src/models/process_person.py
@@ -0,0 +1,12 @@
+from typing import Literal
+
+from pydantic import BaseModel
+
+from .py_object_id import PyObjectId
+
+
+class ProcessPerson(BaseModel):
+    name: str
+    role: Literal['jockey', 'owner', 'trainer']
+    race_id: PyObjectId
+    runner_id: PyObjectId
