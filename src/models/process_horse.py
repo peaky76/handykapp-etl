b15064bbb7e84434738a43e67c53bbdf3cca6a01
peaky76 <robertjamespeacock@gmail.com>
2024-03-08 22:40:08 +0000

    refactor: improve ProcessHorse type

--- src/models/process_horse.py
+++ src/models/process_horse.py
@@ -1,6 +1,22 @@
-from .mongo_horse import MongoHorse
+from typing import List, Literal, Optional
+
+from pydantic import BaseModel, Field
+
+from .mongo_horse import MongoOfficialRatings, MongoOperation
 from .py_object_id import PyObjectId
 
 
-class ProcessHorse(MongoHorse):
+class ProcessHorse(BaseModel):
+    name: str = Field(..., min_length=3, max_length=21)
+    country: str = Field(..., min_length=2, max_length=3)
+    year: int
+    sex: Optional[Literal["M", "F"]] = None
+    breed: Optional[str] = None
+    colour: Optional[str] = None
+    sire: Optional[str] = None
+    dam: Optional[str] = None
+    jockey: Optional[str] = None
+    trainer: Optional[str] = None
+    operations: Optional[List[MongoOperation]] = None
+    ratings: Optional[MongoOfficialRatings] = None
     race_id: PyObjectId
