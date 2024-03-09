5918894f17f362aa616ca884df7deef1e3afe80f
peaky76 <robertjamespeacock@gmail.com>
2024-03-09 23:33:37 +0000

    refactor: make ProcessHorseCore hashable

--- src/models/process_horse_core.py
+++ src/models/process_horse_core.py
@@ -7,3 +7,6 @@ class ProcessHorseCore(BaseModel):
     name: str = Field(..., min_length=3, max_length=21)
     country: str = Field(..., min_length=2, max_length=3)
     sex: Optional[Literal["M", "F"]] = None
+
+    def __hash__(self):
+        return hash(tuple(self.__dict__.values()))
