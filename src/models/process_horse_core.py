c1ca926cb320aa566c148416c03c1a2ba15b9ad6
peaky76 <robertjamespeacock@gmail.com>
2024-03-10 20:16:18 +0000

    refactor: add year to ProcessHorseCore

--- src/models/process_horse_core.py
+++ src/models/process_horse_core.py
@@ -7,3 +7,4 @@ class ProcessHorseCore(BaseModel):
     name: str = Field(..., min_length=3, max_length=21)
     country: str = Field(..., min_length=2, max_length=3)
     sex: Optional[Literal["M", "F"]] = None
+    year: Optional[int] = None
