d2097cc7a11772b03de7936569de4f4f3cdd717c
peaky76 <robertjamespeacock@gmail.com>
2024-03-10 16:53:42 +0000

    refactor: specify __eq__  on HashableBaseModel

--- src/models/hashable_base_model.py
+++ src/models/hashable_base_model.py
@@ -5,3 +5,8 @@ class HashableBaseModel(BaseModel):
     def __hash__(self):
         values = tuple(tuple(v) if isinstance(v, list) else v for v in self.__dict__.values())
         return hash((type(self), *values))
+
+    def __eq__(self, other):
+        if isinstance(other, HashableBaseModel):
+            return self.__dict__ == other.__dict__
+        return False
