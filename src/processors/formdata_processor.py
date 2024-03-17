144d4c566d737c3664a3297272ab061065daab0c
peaky76 <robertjamespeacock@gmail.com>
2024-03-17 16:42:03 +0000

    fix: typing in FormdataProcessor

--- src/processors/formdata_processor.py
+++ src/processors/formdata_processor.py
@@ -11,11 +11,12 @@ class FormdataProcessor(DatabaseProcessor[FormdataEntry]):
 
     def update(self, entry: FormdataEntry) -> None:
         existing_entry = self._table.find_one({"_id": self.current_id})
-        existing_runs = existing_entry["runs"]
+        existing_runs = existing_entry["runs"] if existing_entry else []
         incoming_runs = [r.model_dump() for r in entry.runs]
         merged_runs = {d["date"]: d for d in existing_runs + incoming_runs}
 
-        update_dict = {"runs": list(merged_runs.values())} | entry.model_dump(include=["prize_money", "trainer", "trainer_form"])
+        update_keys = {"prize_money", "trainer", "trainer_form"}
+        update_dict = {"runs": list(merged_runs.values())} | entry.model_dump(include=update_keys)
         self._table.update_one(
             {"_id": self.current_id},
             {"$set": update_dict},
