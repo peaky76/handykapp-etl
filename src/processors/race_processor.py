dfd3cf77f4355b245fbb40c42c9f7f3270f88754
peaky76 <robertjamespeacock@gmail.com>
2024-03-17 16:23:36 +0000

    fix: typing in RaceProcessor

--- src/processors/race_processor.py
+++ src/processors/race_processor.py
@@ -1,7 +1,6 @@
 from typing import ClassVar, List, Set
 
-from models import Race
-from models.horse_core import HorseCore
+from models import Declaration, HorseCore
 from prefect import get_run_logger
 
 from processors.horse_processor import HorseProcessor
@@ -10,12 +9,12 @@ from .database_processor import DatabaseProcessor
 from .processor import Processor
 
 
-class RaceProcessor(DatabaseProcessor[Race]):
+class RaceProcessor(DatabaseProcessor[Declaration]):
     _forward_processors: ClassVar[List[Processor]] = [HorseProcessor()]
     _search_keys: ClassVar[Set[str]] = {"racecourse_id", "datetime"}
     _update_keys: ClassVar[Set[str]] = {"rapid_id", "going_description"}
     
-    def post_process(self, race: Race) -> None:
+    def post_process(self, race: Declaration) -> None:
         h = self.running_processors[0]
         try:
             for horse in race.runners:
