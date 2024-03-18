17624cda03464a095f9a6047962ecdf28fca9b43
peaky76 <robertjamespeacock@gmail.com>
2024-03-18 23:34:01 +0000

    fix: PyObjectId outputs proper ObjectId

--- src/models/py_object_id.py
+++ src/models/py_object_id.py
@@ -1,5 +1,27 @@
-from typing import Annotated
+from typing import Any
 
-from pydantic import BeforeValidator
+from bson import ObjectId
+from pydantic_core import core_schema
 
-PyObjectId = Annotated[str, BeforeValidator(str)]
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, value: Any) -> ObjectId:
+        if isinstance(value, ObjectId):
+            return value
+        if isinstance(value, str) and ObjectId.is_valid(value):
+            return ObjectId(value)
+        raise ValueError("Invalid ObjectId")
+    
+    @classmethod
+    def __get_pydantic_core_schema__(
+            cls, _source_type: Any, _handler: Any
+    ) -> core_schema.CoreSchema:
+        return core_schema.json_or_python_schema(
+            json_schema=core_schema.str_schema(),
+            python_schema=core_schema.is_instance_schema(ObjectId)
+        )
