427c9da81d4db448a42ae78c89a5e4d9ea0b4c93
peaky76 <robertjamespeacock@gmail.com>
2024-03-20 21:32:45 +0000

    fix: make jockey a Person object

--- src/models/runner.py
+++ src/models/runner.py
@@ -1,13 +1,14 @@
 from typing import Optional
 
 from .horse import Horse
+from .person import Person
 
 
 class Runner(Horse):
-    jockey: Optional[str] = None
+    jockey: Optional[Person] = None
     allowance: Optional[int] = None
     saddlecloth: Optional[int] = None
     draw: Optional[int] = None
     headgear: Optional[str] = None
-    lbs_carried: Optional[int] = None 
+    lbs_carried: Optional[int] = None
     official_rating: Optional[int] = None
