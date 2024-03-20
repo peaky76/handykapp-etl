d9c6b34f086d09593183b0b08c927f0522a4e623
peaky76 <robertjamespeacock@gmail.com>
2024-03-20 21:33:58 +0000

    fix: add AW as option on SurfaceType

--- src/models/racecourse_fields.py
+++ src/models/racecourse_fields.py
@@ -1,7 +1,7 @@
 from typing import Literal
 
 CodeType = Literal["Flat", "NH"]
-SurfaceType = Literal["Turf", "Dirt", "Fibresand", "Polytrack", "Tapeta", "Sand", "Snow"]
+SurfaceType = Literal["Turf", "Dirt", "AW", "Fibresand", "Polytrack", "Tapeta", "Sand", "Snow"]
 ObstacleType = Literal["Hurdle", "Chase"]
 ShapeType = Literal["Circle", "Horseshoe", "Oval", "Pear", "Triangle"]
 Handedness = Literal["Left", "Right", "Both", "Neither"]
