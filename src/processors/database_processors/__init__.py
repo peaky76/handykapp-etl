a6af97b980e71b955d8fa4f691c977354a7e4675
peaky76 <robertjamespeacock@gmail.com>
2024-03-21 00:32:12 +0000

    refactor: remove unneeded RaceProcessor

--- src/processors/database_processors/__init__.py
+++ src/processors/database_processors/__init__.py
@@ -3,7 +3,6 @@ from .formdata_processor import FormdataProcessor
 from .horse_core_processor import HorseCoreProcessor
 from .horse_processor import HorseProcessor
 from .person_processor import PersonProcessor
-from .race_processor import RaceProcessor
 from .racecourse_processor import RacecourseProcessor
 
 __all__ = [
@@ -12,6 +11,5 @@ __all__ = [
     "HorseCoreProcessor",
     "HorseProcessor",
     "PersonProcessor",
-    "RaceProcessor",
     "RacecourseProcessor",
 ]
