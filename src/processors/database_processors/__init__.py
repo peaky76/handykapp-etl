120fb781af294e8b0aca8426db67c5a574c855b1
peaky76 <robertjamespeacock@gmail.com>
2024-03-25 21:05:08 +0000

    fix: import ResultProcessor into package

--- src/processors/database_processors/__init__.py
+++ src/processors/database_processors/__init__.py
@@ -4,6 +4,7 @@ from .horse_core_processor import HorseCoreProcessor
 from .horse_processor import HorseProcessor
 from .person_processor import PersonProcessor
 from .racecourse_processor import RacecourseProcessor
+from .result_processor import ResultProcessor
 
 __all__ = [
     "DeclarationProcessor",
@@ -12,4 +13,5 @@ __all__ = [
     "HorseProcessor",
     "PersonProcessor",
     "RacecourseProcessor",
+    "ResultProcessor",
 ]
