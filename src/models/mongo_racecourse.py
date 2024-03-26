f93f1406f50e7647747e87a014414cc0492ad336
peaky76 <robertjamespeacock@gmail.com>
2024-03-26 20:13:49 +0000

    refactor: decouple MongoRacecourse and Racecourse

--- src/models/mongo_racecourse.py
+++ src/models/mongo_racecourse.py
@@ -1,6 +1,31 @@
+from typing import Optional
+
+from pydantic import Field
+
 from .mongo_base_model import MongoBaseModel
-from .racecourse import Racecourse
+from .racecourse_fields import (
+    CodeType,
+    ContourType,
+    Handedness,
+    ObstacleType,
+    ShapeType,
+    StyleType,
+    SurfaceType,
+)
+from .references import References
+from .source import Source
 
 
-class MongoRacecourse(MongoBaseModel, Racecourse):
-    pass
+class MongoRacecourse(MongoBaseModel):
+    name: str
+    formal_name: Optional[str] = None
+    country: str = Field(..., min_length=2, max_length=3)
+    code: Optional[CodeType] = None
+    surface: Optional[SurfaceType] = None
+    obstacle: Optional[ObstacleType] = None
+    shape: Optional[ShapeType] = None
+    handedness: Optional[Handedness] = None
+    style: Optional[StyleType] = None
+    contour: Optional[ContourType] = None
+    references: Optional[References] = None
+    source: Source
