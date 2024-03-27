dd50993c4082f4b403fbaa6a0e0f0e6f24ce6879
peaky76 <robertjamespeacock@gmail.com>
2024-03-27 15:55:18 +0000

    fix: HorseCore does correct find

--- src/processors/database_processors/horse_core_processor.py
+++ src/processors/database_processors/horse_core_processor.py
@@ -1,3 +1,5 @@
+from typing import ClassVar, Set
+
 from models import HorseCore, MongoHorse
 
 from .database_processor import DatabaseProcessor
@@ -8,3 +10,4 @@ class HorseCoreProcessor(DatabaseProcessor[HorseCore, MongoHorse]):
     _table_name = "horses"
     _business_model = HorseCore
     _db_model = MongoHorse
+    _search_keys: ClassVar[Set[str]] = {"name", "country", "sex", "year"}
