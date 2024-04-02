d7fa490afb74c8bea56012282a4dc7c421c0149b
peaky76 <robertjamespeacock@gmail.com>
2024-04-02 21:59:02 +0100

    fix: check racecourse source ref first

--- src/processors/database_processors/racecourse_processor.py
+++ src/processors/database_processors/racecourse_processor.py
@@ -8,11 +8,15 @@ class RacecourseProcessor(DatabaseProcessor[Racecourse, MongoRacecourse]):
     _db_model = MongoRacecourse
 
     def find(self, racecourse: MongoRacecourse) -> MongoRacecourse | None:
-        search_dictionary = compact({ 
-            "name": racecourse.name.title(), 
-            "country": racecourse.country, 
-            "obstacle": racecourse.obstacle, 
-            "surface": {"$in": ["Tapeta", "Polytrack"]} if racecourse.surface == "AW" else racecourse.surface 
-         })
-        db_item = self._table.find_one(search_dictionary)
+        db_item = self._table.find_one({f"references.{racecourse.source}": racecourse.references[racecourse.source]})
+        
+        if not db_item:
+            search_dictionary = compact({ 
+                "name": racecourse.name.title(), 
+                "country": racecourse.country, 
+                "obstacle": racecourse.obstacle, 
+                "surface": {"$in": ["Tapeta", "Polytrack"]} if racecourse.surface == "AW" else racecourse.surface 
+            })
+            db_item = self._table.find_one(search_dictionary)
+            
         return MongoRacecourse(**dict(db_item | {"db_id": db_item["_id"]})) if db_item else None
