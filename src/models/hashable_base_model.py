7df34fcdf241a7a96567b415f20bf7962585ce95
peaky76 <robertjamespeacock@gmail.com>
2024-04-03 21:22:33 +0100

    fix: amend HashableBaseModel to hash internal dicts

--- src/models/hashable_base_model.py
+++ src/models/hashable_base_model.py
@@ -3,7 +3,11 @@ from pydantic import BaseModel
 
 class HashableBaseModel(BaseModel):
     def __hash__(self):
-        values = tuple(tuple(v) if isinstance(v, list) else v for v in self.__dict__.values())
+        values = tuple(
+            tuple(v.items()) if isinstance(v, dict) else
+            tuple(v) if isinstance(v, list) else
+            v for v in self.__dict__.values()
+        )
         return hash((type(self), *values))
 
     def __eq__(self, other):
