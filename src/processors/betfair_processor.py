66a827c3cce19b7569dc9d87647aae3096c85387
peaky76 <robertjamespeacock@gmail.com>
2024-06-10 22:43:54 +0100

    feat: betfair loader and processor

---
+++ src/processors/betfair_processor.py
@@ -0,0 +1,33 @@
+from clients import mongo_client as client
+from prefect import get_run_logger
+from pymongo.errors import DuplicateKeyError
+
+db = client.handykapp
+
+
+def betfair_processor():
+    logger = get_run_logger()
+    logger.info("Starting betfair processor")
+    updated_count = 0
+    added_count = 0
+    skipped_count = 0
+
+    try:
+        while True:
+            pnl_line = yield
+            
+            try:
+                inserted_pnl_line = db.betfair.insert_one(
+                    pnl_line.model_dump()
+                )
+                found_id = inserted_pnl_line.inserted_id
+                logger.debug(f"{pnl_line.racecourse} {pnl_line.race_datetime} P&L added to db")
+                added_count += 1
+            except DuplicateKeyError:
+                logger.warning(f"Duplicate P&L line: {pnl_line.racecourse} {pnl_line.race_datetime}")
+                skipped_count += 1
+
+    except GeneratorExit:
+        logger.info(
+            f"Finished processing Betfair P&L. Updated {updated_count}, added {added_count}, skipped {skipped_count}"
+        )
