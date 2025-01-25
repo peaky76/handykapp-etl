bc4febf4c6604ed8de4a52aece8f10ca11ad9a29
peaky76 <robertjamespeacock@gmail.com>
2025-01-25 21:19:09 +0000

    feat: transform_betfair_bet_history fn

--- src/transformers/betfair_transformer.py
+++ src/transformers/betfair_transformer.py
@@ -10,8 +10,10 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 import tomllib
 from helpers import get_files, log_validation_problem, stream_file
+from models.mongo_betfair_horserace_bet_history import MongoBetfairHorseraceBetHistory
 from models.mongo_betfair_horserace_pnl import MongoBetfairHorseracePnl
 from prefect import flow, get_run_logger, task
+from pybet import Odds
 
 with open("settings.toml", "rb") as f:
     settings = tomllib.load(f)
@@ -34,6 +36,67 @@ def read_csv(csv):
     source = petl.MemorySource(stream_file(csv))
     return petl.fromcsv(source, encoding="utf-8")
 
+
+@task(tags=["Betfair"])
+def transform_betfair_bet_history(data: petl.Table) -> List[MongoBetfairHorseraceBetHistory]:
+    transformed_data = (
+        petl.rename(data, {
+            "Market": "market",
+            "Selection": "horse",
+            "Bid Type": "bet_type",
+            "Avg. odds matched": "odds",
+            "Stake (£)": "stake",
+            "Profit/Loss (£)": "profit_loss"
+        })
+        .convert("odds", lambda x: Odds(x))
+        .convert("stake", float)
+        .convert("profit_loss", lambda x: float(x.replace("(", "-").replace(")", "")))
+        .addfield("sport", lambda rec: rec["market"].split("/")[0].strip())
+        .addfield("market_detail", lambda rec: rec["market"].split("/")[1].strip())
+        .addfield("racecourse_and_datetime", lambda rec: rec["market_detail"].split(" : ")[0].strip())
+        .addfield("place_detail", lambda rec: rec["market_detail"].split(" : ")[1].strip())
+        .addfield("racecourse", lambda rec: re.split(r" (?=\d)", rec["racecourse_and_datetime"])[0].strip())
+        .select(lambda rec: rec["sport"] == "Horse Racing")
+        .addfield("is_place_market", lambda rec: "TBP" in rec["place_detail"] or "Placed" in rec["place_detail"])
+        .addfield("places", lambda rec: 1 if not rec["is_place_market"] else int(re.search(r"\d+", rec["place_detail"]).group()) if "TBP" in rec["place_detail"] else None)
+        .addfield("race_description", lambda rec: rec["place_detail"] if not rec["is_place_market"] else None)
+        .cutout("market", "market_detail", "racecourse_and_datetime", "place_detail", "is_place_market")
+        .dicts()
+    )
+
+    filled_out_data = []
+    for rec in transformed_data:
+        if not rec["race_description"]:    
+            matching_description = next((other_rec["race_description"] for other_rec in transformed_data if other_rec["race_datetime"] == rec["race_datetime"] and other_rec["race_description"]), None)
+            rec["race_description"] = matching_description
+        filled_out_data.append(rec)
+
+    return [MongoBetfairHorseraceBetHistory(**rec) for rec in filled_out_data]
+
+@task(tags=["Betfair"])
+def validate_betfair_bet_history(data: petl.Table) -> bool:
+    header = (
+        "\ufeffMarket",
+        "Selection",
+        "Bid type",
+        "Avg. odds matched",
+        "Stake (£)",
+        "Profit/Loss (£)"
+    )
+
+    validate_betfair_date = lambda x: re.match(r"\d{2}-[A-Za-z]{3}-\d{2} \d{2}:\d{2}", x)
+
+    constraints = [
+        {"name": "market_str", "field": "\ufeffMarket", "assertion": str},
+        {"name": "horse_str", "field": "Selection", "assertion": str},
+        {"name": "bid_type_str", "field": "Bid type", "assertion": lambda x: x in ("BACK", "LAY")},
+        {"name": "odds_float", "field": "Avg. odds matched", "assertion": float},
+        {"name": "stake_float", "field": "Stake (£)", "assertion": float},
+        {"name": "start_time_valid", "field": "Start time", "assertion": validate_betfair_date},
+        {"name": "pnl_float", "field": "Profit/Loss (£)", "assertion": lambda x: x == 0.00 or float(x)},
+    ]
+    validator = {"header": header, "constraints": constraints}
+    return petl.validate(data, **validator)
 
 @task(tags=["Betfair"])
 def transform_betfair_pnl_data(data: petl.Table) -> List[MongoBetfairHorseracePnl]:
