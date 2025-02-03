f2c30d34dd8d050925c34f6dd01cea7b39609309
peaky76 <robertjamespeacock@gmail.com>
2025-02-03 20:57:02 +0000

    fix: typing in spaces_client using asyncio

--- src/clients/spaces_client.py
+++ src/clients/spaces_client.py
@@ -1,11 +1,19 @@
+import asyncio
+
 import boto3  # type: ignore
 from prefect.blocks.system import Secret
 
-session = boto3.session.Session()
-spaces_client = session.client(
-    "s3",
-    region_name="ams3",
-    endpoint_url="https://ams3.digitaloceanspaces.com",
-    aws_access_key_id=Secret.load("spaces-key").get(),
-    aws_secret_access_key=Secret.load("spaces-secret").get(),
-)
+
+async def create_spaces_client() -> boto3.client:
+    session = boto3.session.Session()
+    spaces_key = await Secret.load("spaces-key")
+    spaces_secret = await Secret.load("spaces-secret")
+    return session.client(
+        "s3",
+        region_name="ams3",
+        endpoint_url="https://ams3.digitaloceanspaces.com",
+        aws_access_key_id=spaces_key.get(),
+        aws_secret_access_key=spaces_secret.get(),
+    )
+
+spaces_client = asyncio.run(create_spaces_client())
