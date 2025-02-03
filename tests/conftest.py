fa211a2838a89c784105ec029fc59d1164bc0aa9
peaky76 <robertjamespeacock@gmail.com>
2025-02-03 22:40:40 +0000

    fix: ruff PTH errors

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
-import os
 import sys
+from pathlib import Path
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+# Add the src directory to the Python path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent / 'src'))
