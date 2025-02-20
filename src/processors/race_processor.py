cd66db9b21de1a4a4f92ae8ac20550ac43e00972
peaky76 <robertjamespeacock@gmail.com>
2025-02-20 21:47:16 +0000

    refactor: log if no racecourse found

--- src/processors/race_processor.py
+++ src/processors/race_processor.py
@@ -133,6 +133,10 @@ def race_processor():
                             r.send((horse | {"race_id": race_id}, source))
                 except Exception as e:
                     logger.error(f"Error processing {race_id}: {e}")
+            else:
+                logger.error(
+                    f"Unable to add race {race_id}: No matching racecourse found"
+                )
 
     except GeneratorExit:
         logger.info(
