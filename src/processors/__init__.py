711a4156a22b42a38901bcb23f9cb7fd2f39e2d5
peaky76 <robertjamespeacock@gmail.com>
2025-02-20 22:05:11 +0000

    refactor: processors exports

---
+++ src/processors/__init__.py
@@ -0,0 +1,13 @@
+from .betfair_processor import betfair_processor
+from .person_processor import person_processor
+from .race_processor import race_processor
+from .record_processor import record_processor
+from .runner_processor import runner_processor
+
+__all__ = [
+    "betfair_processor",
+    "person_processor",
+    "race_processor",
+    "record_processor",
+    "runner_processor",
+]
