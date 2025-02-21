943142f10d6b4137c9f8458dba112b08d80411f4
peaky76 <robertjamespeacock@gmail.com>
2025-02-21 22:56:27 +0000

    feat: FormdataRace and Runner classes

---
+++ src/models/formdata_race.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class FormdataRace(BaseModel):
+    date: str = Field(..., pattern=r"^\d{4}-\d{2}-\d{2}$")
+    race_type: str
+    win_prize: str
+    course: str = Field(..., min_length=3, max_length=3)
+    number_of_runners: int
+    distance: Optional[float] = None
+    going: Optional[str] = None
---
+++ src/models/formdata_runner.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class FormdataRunner(BaseModel):
+    name: str = Field(..., min_length=3, max_length=21)
+    country: str = Field(..., min_length=2, max_length=3)
+    year: int
+    weight: str = Field(..., pattern=r"^\d{1,2}-\d{1,2}$")
+    headgear: Optional[str] = None
+    allowance: Optional[int] = None
+    jockey: str
+    position: str
+    beaten_distance: Optional[float] = None
+    time_rating: Optional[int] = None
+    form_rating: Optional[int] = None
