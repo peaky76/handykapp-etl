74fd844379329353471054068e262942fd3a9de6
peaky76 <robertjamespeacock@gmail.com>
2025-02-24 17:10:37 +0000

    refactor: change return type of check_race_complete

--- tests/processors/formdata_processors/test_race_builder.py
+++ tests/processors/formdata_processors/test_race_builder.py
@@ -10,19 +10,15 @@ def race(mocker):
     return race
 
 
-def test_check_race_complete_fails_when_not_enough_runners(race):
+def test_check_race_complete_when_not_enough_runners(race):
     runners = []
-    assert check_race_complete(race, runners) is False
+    actual = check_race_complete(race, runners)
+
+    assert actual["complete"] == []
+    assert actual["todo"] == runners
 
 
-def test_check_race_complete_fails_when_too_many_runners(mocker, race):
-    runners = [mocker.patch("models.FormdataRunner")] * 7
-    assert check_race_complete(race, runners) is False
-
-
-def test_check_race_complete_succeeds_when_correct_number_of_runners_in_valid_rank(
-    mocker, race
-):
+def test_check_race_complete_when_correct_number_of_runners_in_valid_rank(mocker, race):
     runners = [
         mocker.patch("models.FormdataRunner", position="5"),
         mocker.patch("models.FormdataRunner", position="3"),
@@ -31,10 +27,13 @@ def test_check_race_complete_succeeds_when_correct_number_of_runners_in_valid_ra
         mocker.patch("models.FormdataRunner", position="2"),
         mocker.patch("models.FormdataRunner", position="4"),
     ]
-    assert check_race_complete(race, runners) is True
+    actual = check_race_complete(race, runners)
+
+    assert actual["complete"] == runners
+    assert actual["todo"] == []
 
 
-def test_check_race_complete_fails_when_correct_number_of_runners_in_invalid_rank(
+def test_check_race_complete_when_correct_number_of_runners_in_invalid_rank(
     mocker, race
 ):
     runners = [
@@ -45,10 +44,13 @@ def test_check_race_complete_fails_when_correct_number_of_runners_in_invalid_ran
         mocker.patch("models.FormdataRunner", position="2"),
         mocker.patch("models.FormdataRunner", position="4"),
     ]
-    assert check_race_complete(race, runners) is False
+    actual = check_race_complete(race, runners)
+
+    assert actual["complete"] == []
+    assert actual["todo"] == runners
 
 
-def test_check_race_complete_succeeds_when_correct_number_of_runners_in_valid_rank_and_some_equal(
+def test_check_race_complete_when_correct_number_of_runners_in_valid_rank_and_some_equal(
     mocker, race
 ):
     runners = [
@@ -59,10 +61,13 @@ def test_check_race_complete_succeeds_when_correct_number_of_runners_in_valid_ra
         mocker.patch("models.FormdataRunner", position="2"),
         mocker.patch("models.FormdataRunner", position="6"),
     ]
-    assert check_race_complete(race, runners) is True
+    actual = check_race_complete(race, runners)
+
+    assert actual["complete"] == runners
+    assert actual["todo"] == []
 
 
-def test_check_race_complete_succeeds_when_correct_number_of_runners_in_invalid_rank_and_some_equal(
+def test_check_race_complete_when_correct_number_of_runners_in_invalid_rank_and_some_equal(
     mocker, race
 ):
     runners = [
@@ -73,10 +78,13 @@ def test_check_race_complete_succeeds_when_correct_number_of_runners_in_invalid_
         mocker.patch("models.FormdataRunner", position="2"),
         mocker.patch("models.FormdataRunner", position="7"),
     ]
-    assert check_race_complete(race, runners) is False
+    actual = check_race_complete(race, runners)
+
+    assert actual["complete"] == []
+    assert actual["todo"] == runners
 
 
-def test_check_race_complete_succeeds_when_correct_number_of_runners_in_valid_rank_with_non_completion(
+def test_check_race_complete_when_correct_number_of_runners_in_valid_rank_with_non_completion(
     mocker, race
 ):
     runners = [
@@ -87,10 +95,13 @@ def test_check_race_complete_succeeds_when_correct_number_of_runners_in_valid_ra
         mocker.patch("models.FormdataRunner", position="2"),
         mocker.patch("models.FormdataRunner", position="4"),
     ]
-    assert check_race_complete(race, runners) is True
+    actual = check_race_complete(race, runners)
+
+    assert actual["complete"] == runners
+    assert actual["todo"] == []
 
 
-def test_check_race_complete_succeeds_when_correct_number_of_runners_in_invalid_rank_with_non_completion(
+def test_check_race_complete_when_correct_number_of_runners_in_invalid_rank_with_non_completion(
     mocker, race
 ):
     runners = [
@@ -101,4 +112,7 @@ def test_check_race_complete_succeeds_when_correct_number_of_runners_in_invalid_
         mocker.patch("models.FormdataRunner", position="2"),
         mocker.patch("models.FormdataRunner", position="P"),
     ]
-    assert check_race_complete(race, runners) is False
+    actual = check_race_complete(race, runners)
+
+    assert actual["complete"] == []
+    assert actual["todo"] == runners
