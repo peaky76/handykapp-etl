d66754de2149d5e6f2bcb4d46bf3c2c2ce13ca58
peaky76 <robertjamespeacock@gmail.com>
2025-03-01 21:31:17 +0000

    feat: MongoRunner model

---
+++ src/models/mongo_runner.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from pendulum import Duration
+from pydantic import BaseModel
+
+from models.py_object_id import PyObjectId
+
+
+class MongoRunner(BaseModel):
+    horse: PyObjectId
+    owner: Optional[str] = None
+    lbs_carried: Optional[int] = None
+    allowance: Optional[int] = None
+    saddlecloth: Optional[int] = None
+    draw: Optional[int] = None
+    headgear: Optional[str] = None
+    official_rating: Optional[int] = None
+    trainer: Optional[PyObjectId] = None
+    jockey: Optional[PyObjectId] = None
+    position: Optional[str] = None
+    beaten_distance: Optional[str] = None
+    time: Optional[Duration] = None
---
+++ tests/models/test_mongo_runner.py
@@ -0,0 +1,35 @@
+import pytest
+from bson import ObjectId
+from pydantic import ValidationError
+
+from models.mongo_runner import MongoRunner
+from models.py_object_id import PyObjectId
+
+
+def test_mongo_runner_init_with_sufficient_fields():
+    assert MongoRunner(horse=PyObjectId(ObjectId()))
+
+
+def test_mongo_runner_init_with_insufficient_fields():
+    with pytest.raises(ValidationError):
+        MongoRunner()
+
+
+def test_mongo_runner_init_with_incorrect_horse_id():
+    with pytest.raises(ValidationError):
+        MongoRunner(horse="not_an_object_id")
+
+
+def test_mongo_runner_init_with_optional_fields():
+    horse_id = PyObjectId(ObjectId())
+    assert MongoRunner(
+        horse=horse_id,
+        lbs_carried=120,
+        allowance=3,
+        saddlecloth=1,
+        draw=5,
+        headgear="blinkers",
+        official_rating=85,
+        position="1",
+        beaten_distance="0.5",
+    )
