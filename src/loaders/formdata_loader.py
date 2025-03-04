04547e6108b510afda2139a7b409368afd77a5e1
peaky76 <robertjamespeacock@gmail.com>
2025-03-04 21:27:31 +0000

    refactor: formdata_loader -> entry_processor

--- src/loaders/formdata_loader.py
+++ src/loaders/formdata_loader.py
@@ -12,7 +12,6 @@ from peak_utility.text.case import normal  # type: ignore
 from prefect import flow, get_run_logger
 
 from clients import mongo_client as client
-from models.formdata_horse import FormdataHorse
 from processors.formdata_processors import file_processor
 from transformers.formdata_transformer import get_formdatas
 
@@ -45,58 +44,6 @@ def create_code_to_course_dict():
         racecourse["references"]["racing_research"]: racecourse["_id"]
         for racecourse in source
     }
-
-
-def formdata_loader():
-    logger = get_run_logger()
-    logger.info("Starting formdata loader")
-
-    try:
-        while True:
-            item = yield
-            horse = item
-
-            existing_entry = db.formdata.find_one(
-                {
-                    "name": horse.name,
-                    "country": horse.country,
-                    "year": horse.year,
-                }
-            )
-
-            if existing_entry:
-                existing_horse = FormdataHorse.model_validate(existing_entry)
-                runs = existing_horse.runs
-
-                for new_run in horse.runs:
-                    matched_run = next(
-                        (r for r in runs if r.date == new_run.date),
-                        None,
-                    )
-                    if matched_run:
-                        runs.remove(matched_run)
-                    runs.append(new_run)
-
-                db.formdata.find_one_and_update(
-                    {
-                        "name": horse.name,
-                        "country": horse.country,
-                        "year": horse.year,
-                    },
-                    {
-                        "$set": {
-                            "runs": [run.model_dump() for run in runs],
-                            "prize_money": horse.prize_money,
-                            "trainer": horse.trainer,
-                            "trainer_form": horse.trainer_form,
-                        }
-                    },
-                )
-            else:
-                db.formdata.insert_one(horse.model_dump())
-
-    except GeneratorExit:
-        pass
 
 
 @flow
---
+++ src/processors/formdata_processors/entry_processor.py
@@ -0,0 +1,58 @@
+from prefect import get_run_logger
+
+from clients import mongo_client as client
+from models.formdata_horse import FormdataHorse
+
+db = client.handykapp
+
+
+def entry_processor():
+    logger = get_run_logger()
+    logger.info("Starting entry processor")
+
+    try:
+        while True:
+            item = yield
+            horse = item
+
+            existing_entry = db.formdata.find_one(
+                {
+                    "name": horse.name,
+                    "country": horse.country,
+                    "year": horse.year,
+                }
+            )
+
+            if existing_entry:
+                existing_horse = FormdataHorse.model_validate(existing_entry)
+                runs = existing_horse.runs
+
+                for new_run in horse.runs:
+                    matched_run = next(
+                        (r for r in runs if r.date == new_run.date),
+                        None,
+                    )
+                    if matched_run:
+                        runs.remove(matched_run)
+                    runs.append(new_run)
+
+                db.formdata.find_one_and_update(
+                    {
+                        "name": horse.name,
+                        "country": horse.country,
+                        "year": horse.year,
+                    },
+                    {
+                        "$set": {
+                            "runs": [run.model_dump() for run in runs],
+                            "prize_money": horse.prize_money,
+                            "trainer": horse.trainer,
+                            "trainer_form": horse.trainer_form,
+                        }
+                    },
+                )
+            else:
+                db.formdata.insert_one(horse.model_dump())
+
+    except GeneratorExit:
+        pass
