a1c0ef8e4bbf609cbf293375f3d31f5f35ece77f
peaky76 <robertjamespeacock@gmail.com>
2025-03-29 14:15:14 +0000

    refactor: use bulk operations in EntryProcessor

--- src/processors/formdata_processors/entry_processor.py
+++ src/processors/formdata_processors/entry_processor.py
@@ -1,4 +1,5 @@
 from prefect import get_run_logger
+from pymongo import InsertOne, UpdateOne
 
 from clients import mongo_client as client
 from models.formdata_horse import FormdataHorse
@@ -10,9 +11,14 @@ def entry_processor():
     logger = get_run_logger()
     logger.info("Starting entry processor")
 
+    bulk_operations = []
+    bulk_threshold = 50
+    processed_count = 0
+
     try:
         while True:
             horse = yield
+            processed_count += 1
 
             existing_entry = db.formdata.find_one(
                 {
@@ -35,23 +41,36 @@ def entry_processor():
                         runs.remove(matched_run)
                     runs.append(new_run)
 
-                db.formdata.find_one_and_update(
-                    {
-                        "name": horse.name,
-                        "country": horse.country,
-                        "year": horse.year,
-                    },
-                    {
-                        "$set": {
-                            "runs": [run.model_dump() for run in runs],
-                            "prize_money": horse.prize_money,
-                            "trainer": horse.trainer,
-                            "trainer_form": horse.trainer_form,
-                        }
-                    },
+                bulk_operations.append(
+                    UpdateOne(
+                        {
+                            "name": horse.name,
+                            "country": horse.country,
+                            "year": horse.year,
+                        },
+                        {
+                            "$set": {
+                                "runs": [run.model_dump() for run in runs],
+                                "prize_money": horse.prize_money,
+                                "trainer": horse.trainer,
+                                "trainer_form": horse.trainer_form,
+                            }
+                        },
+                    )
                 )
             else:
-                db.formdata.insert_one(horse.model_dump())
+                bulk_operations.append(InsertOne(horse.model_dump()))
+
+            # Execute bulk operations when threshold is reached
+            if len(bulk_operations) >= bulk_threshold:
+                db.formdata.bulk_write(bulk_operations)
+                bulk_operations = []
+
+            if processed_count % 100 == 0:
+                logger.info(f"Processed {processed_count} horses")
 
     except GeneratorExit:
-        pass
+        # Process remaining operations
+        if bulk_operations:
+            db.formdata.bulk_write(bulk_operations)
+        logger.info(f"Completed processing {processed_count} horses")
