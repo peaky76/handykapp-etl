0023d05f6f089f60a1ef859f8fb8e673ae132918
peaky76 <robertjamespeacock@gmail.com>
2025-03-29 14:32:31 +0000

    refactor: extra indexes on db

--- src/loaders/main_loader.py
+++ src/loaders/main_loader.py
@@ -8,12 +8,14 @@ from prefect import flow, task
 from pymongo import ASCENDING as ASC
 
 from clients import mongo_client as client
-from loaders.bha_loader import load_bha
+
+# from loaders.bha_loader import load_bha
 from loaders.formdata_loader import load_formdata_only
-from loaders.jockey_ratings_loader import load_jockey_ratings
-from loaders.racecourse_loader import load_racecourses
-from loaders.rapid_horseracing_loader import load_rapid_horseracing_data
-from loaders.theracingapi_loader import load_theracingapi_data
+
+# from loaders.jockey_ratings_loader import load_jockey_ratings
+# from loaders.racecourse_loader import load_racecourses
+# from loaders.rapid_horseracing_loader import load_rapid_horseracing_data
+# from loaders.theracingapi_loader import load_theracingapi_data
 
 db = client.handykapp
 
@@ -25,6 +27,9 @@ def drop_database():
 
 @task
 def spec_database():
+    db.formdata.create_index(
+        [("name", ASC), ("country", ASC), ("year", ASC)], unique=True
+    )
     db.horses.create_index(
         [("name", ASC), ("country", ASC), ("year", ASC)],
         unique=True,
@@ -36,8 +41,15 @@ def spec_database():
         [("name", ASC), ("country", ASC), ("year", ASC), ("sex", ASC)], unique=True
     )
     db.horses.create_index("name")
+    db.horses.create_index("sire")
+    db.horses.create_index("dam")
     db.people.create_index(
         [("last", ASC), ("first", ASC), ("middle", ASC)], unique=True
+    )
+    db.people.create_index(
+        "references.racing_research",
+        unique=True,
+        partialFilterExpression={"references.racing_research": {"$exists": True}},
     )
     db.racecourses.create_index(
         [("name", ASC), ("country", ASC), ("obstacle", ASC), ("surface", ASC)],
@@ -45,19 +57,19 @@ def spec_database():
     )
     db.racecourses.create_index("name")
     db.races.create_index([("racecourse", ASC), ("datetime", ASC)], unique=True)
-    db.races.create_index("result.horse")
+    db.races.create_index("runners.horse")
 
 
 @flow
 def load_database_afresh():
-    drop_database()
+    # drop_database()
     spec_database()
-    load_racecourses()
-    load_bha()
+    # load_racecourses()
+    # load_bha()
     load_formdata_only()
-    load_theracingapi_data()
-    load_rapid_horseracing_data()
-    load_jockey_ratings()
+    # load_theracingapi_data()
+    # load_rapid_horseracing_data()
+    # load_jockey_ratings()
     # load_formdata_horses()
 
 
