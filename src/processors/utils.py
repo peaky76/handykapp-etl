5a626947eca118194226b86d4c3a8e41d7552f5f
peaky76 <robertjamespeacock@gmail.com>
2025-05-10 12:15:11 +0100

    feat: log memory usage fn

--- src/processors/utils.py
+++ src/processors/utils.py
@@ -1,2 +1,14 @@
+import os
+
+import psutil
+from prefect import get_run_logger
+
+
 def compact(dictionary: dict):
     return {k: v for k, v in dictionary.items() if v is not None}
+
+
+def log_memory_usage():
+    logger = get_run_logger()
+    process = psutil.Process(os.getpid())
+    logger.info(f"Memory usage: {process.memory_info().rss / 1024**2:.2f} MB")
