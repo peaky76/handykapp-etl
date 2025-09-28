b7f6651c16845948939ebfe09f2b01a35f530b76
peaky76 <robertjamespeacock@gmail.com>
2025-09-28 10:15:44 +0100

    refactor: move test into source_data_runner

--- src/runners/source_data_runner.py
+++ src/runners/source_data_runner.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-from prefect import flow
+from prefect import flow, get_run_logger
 
 from extractors.rapid_horseracing_extractor import (
     rapid_horseracing_extractor,  # type: ignore
@@ -12,7 +12,12 @@ from extractors.rapid_horseracing_extractor import (
 from extractors.theracingapi_extractor import (
     theracingapi_racecards_extractor,  # type: ignore
 )
-from loaders.test_loader import load_nothing_and_log  # type: ignore
+
+
+@flow
+def load_nothing_and_log():
+    logger = get_run_logger()
+    logger.info("I've run, but done nothing.")
 
 
 @flow
