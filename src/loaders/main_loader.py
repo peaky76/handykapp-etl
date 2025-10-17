cce7d19f8b3837cd896d5122c4139c24aa320741
peaky76 <robertjamespeacock@gmail.com>
2025-10-17 22:00:15 +0100

    build: reorg imports

--- src/loaders/main_loader.py
+++ src/loaders/main_loader.py
@@ -2,8 +2,6 @@
 import sys
 from pathlib import Path
 
-from loaders.rapid_horseracing_loader import load_rapid_horseracing_entries
-
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 import pendulum
@@ -14,6 +12,7 @@ from clients import mongo_client as client
 from loaders.bha_loader import load_bha_data
 from loaders.formdata_loader import load_formdata
 from loaders.racecourse_loader import load_racecourses
+from loaders.rapid_horseracing_loader import load_rapid_horseracing_entries
 from loaders.theracingapi_loader import load_theracingapi_data
 
 db = client.handykapp
