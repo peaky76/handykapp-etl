7b34c485f60dbdbe26c8f107fa9974f576c1c6b2
peaky76 <robertjamespeacock@gmail.com>
2025-10-17 22:05:22 +0100

    build: remove dead import

--- src/processors/formdata_processors/__init__.py
+++ src/processors/formdata_processors/__init__.py
@@ -1,6 +1,5 @@
 from .file_processor import file_processor
 from .page_processor import page_processor
-from .race_builder import race_builder
 from .word_processor import word_processor
 
-__all__ = ["file_processor", "page_processor", "race_builder", "word_processor"]
+__all__ = ["file_processor", "page_processor", "word_processor"]
