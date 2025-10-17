3a7690a65562a6f227718ba2226687afe25e32d9
peaky76 <robertjamespeacock@gmail.com>
2025-10-17 21:00:01 +0100

    refactor: PreMongoEntry model as parent of PreMongoRunner

--- src/models/pre_mongo_runner.py
+++ src/models/pre_mongo_runner.py
@@ -2,18 +2,10 @@ from typing import Optional
 
 from pydantic_extra_types.pendulum_dt import Duration
 
-from .pre_mongo_horse import PreMongoHorse
+from .pre_mongo_entry import PreMongoEntry
 
 
-class PreMongoRunner(PreMongoHorse):
-    lbs_carried: Optional[int] = None
-    allowance: Optional[int] = None
-    saddlecloth: Optional[int] = None
-    draw: Optional[int] = None
-    headgear: Optional[str] = None
-    official_rating: Optional[int] = None
-    jockey: Optional[str] = None
-    damsire: Optional[str] = None
+class PreMongoRunner(PreMongoEntry):
     finishing_position: Optional[str] = None
     official_position: Optional[str] = None
     beaten_distance: Optional[float] = None
