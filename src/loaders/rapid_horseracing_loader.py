fcb295cbfa785da51578803b006dd3d46a580bd9
peaky76 <robertjamespeacock@gmail.com>
2025-10-17 21:51:08 +0100

    feat: load_rapid_horseracing_entries fn

--- src/loaders/rapid_horseracing_loader.py
+++ src/loaders/rapid_horseracing_loader.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-
+import pendulum
 import tomllib
 from prefect import flow, get_run_logger
 
@@ -12,7 +12,10 @@ from clients import SpacesClient
 from clients import mongo_client as client
 from models import RapidRecord
 from processors.record_processor import record_processor
-from transformers.rapid_horseracing_transformer import transform_results
+from transformers.rapid_horseracing_transformer import (
+    transform_results,
+    transform_results_as_entries,
+)
 
 with open("settings.toml", "rb") as f:
     settings = tomllib.load(f)
@@ -20,6 +23,29 @@ with open("settings.toml", "rb") as f:
 SOURCE = settings["rapid_horseracing"]["spaces_dir"]
 
 db = client.handykapp
+
+
+@flow
+def load_rapid_horseracing_entries(
+    *, until_date: pendulum.Date = pendulum.now().date()
+):
+    logger = get_run_logger()
+    logger.info("Starting rapid_horseracing entries loader")
+
+    r = record_processor()
+    next(r)
+
+    files = SpacesClient.get_files(f"{SOURCE}results")
+
+    for file in files:
+        if file != "results_to_do_list.json":
+            data = SpacesClient.read_file(file)
+            record = RapidRecord(**data)
+            if pendulum.parse(record.date).date() >= until_date:
+                break
+            r.send((record, transform_results_as_entries, file, "rapid"))
+
+    r.close()
 
 
 @flow
