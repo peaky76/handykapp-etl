e1ef4973d64a99be723c587eef7179529c084b36
peaky76 <robertjamespeacock@gmail.com>
2025-10-19 21:35:10 +0100

    refactor: move runner db fns to mongo_client file

--- src/processors/runner_processor.py
+++ src/processors/runner_processor.py
@@ -1,74 +1,17 @@
 from collections.abc import Generator
-from functools import cache, wraps
 
 from pendulum import Date
 from prefect import get_run_logger
 from pymongo import UpdateOne
 from pymongo.errors import DuplicateKeyError
 
-from clients import mongo_client as client
+from clients.mongo_client import get_dam_id, get_horse, get_sire_id, mongo_client
 from models import MongoHorse, MongoOperation, PreMongoRunner, PyObjectId
 from processors.person_processor import person_processor
 
 from .utils import compact
 
-db = client.handykapp
-
-
-def cache_if_found(func):
-    cache = {}
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        key = (args, tuple(sorted(kwargs.items())))
-        if key in cache:
-            return cache[key]
-        result = func(*args, **kwargs)
-        if result is not None:  # Only cache when we find something
-            cache[key] = result
-        return result
-
-    return wrapper
-
-
-@cache_if_found
-def get_horse(name: str, country: str, year: int, sex: str) -> MongoHorse | None:
-    horse = db.horses.find_one(
-        compact({"name": name, "country": country, "year": year, "sex": sex}),
-    )
-    if horse:
-        return horse
-
-    return db.horses.find_one(
-        compact({"name": name, "country": country, "year": year, "sex": None}),
-    )
-
-
-@cache
-def get_horse_id_by_name_and_sex(
-    name: str | None, sex: str | None
-) -> PyObjectId | None:
-    if not name:
-        return None
-
-    found_horse = db.horses.find_one({"name": name, "sex": sex}, {"_id": 1})
-
-    if not found_horse:
-        raise ValueError(
-            f"Could not find {'fe' if sex == 'F' else ''}male horse {name}"
-        )
-
-    return found_horse["_id"]
-
-
-@cache
-def get_dam_id(name: str | None) -> PyObjectId | None:
-    return get_horse_id_by_name_and_sex(name, "F")
-
-
-@cache
-def get_sire_id(name: str | None) -> PyObjectId | None:
-    return get_horse_id_by_name_and_sex(name, "M")
+db = mongo_client.handykapp
 
 
 def create_gelding_operation(date: Date) -> MongoOperation:
