295576d96110235fe925500b4597f2f422f7dd3a
peaky76 <robertjamespeacock@gmail.com>
2025-10-19 22:21:01 +0100

    refactor: modern typing in PreMongo models

--- src/models/pre_mongo_runner.py
+++ src/models/pre_mongo_runner.py
@@ -1,13 +1,11 @@
-from typing import Optional
-
 from pydantic_extra_types.pendulum_dt import Duration
 
 from .pre_mongo_entry import PreMongoEntry
 
 
 class PreMongoRunner(PreMongoEntry):
-    finishing_position: Optional[str] = None
-    official_position: Optional[str] = None
-    beaten_distance: Optional[float] = None
-    time: Optional[Duration] = None
-    sp: Optional[str] = None
+    finishing_position: str | None = None
+    official_position: str | None = None
+    beaten_distance: float | None = None
+    time: Duration | None = None
+    sp: str | None = None
