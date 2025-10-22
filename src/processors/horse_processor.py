ccfec6611ffaf064279f766f43bba1366ee964b3
peaky76 <robertjamespeacock@gmail.com>
2025-10-22 10:31:50 +0100

    fix: better handle no sire or dam

--- src/processors/horse_processor.py
+++ src/processors/horse_processor.py
@@ -16,8 +16,8 @@ def make_horse_update_dictionary(horse: PreMongoHorse, db_horse: MongoHorse):
     return compact(
         {
             "colour": horse.colour,
-            "sire": x["_id"] if (x := get_horse(horse.sire)) else None,
-            "dam": x["_id"] if (x := get_horse(horse.dam)) else None,
+            "sire": get_horse(horse.sire).id if horse.sire else None,
+            "dam": get_horse(horse.dam).id if horse.dam else None,
             "operations": make_operations_update(horse, db_horse),
             "ratings": horse.ratings,
         }
@@ -32,8 +32,8 @@ def make_horse_insert_dictionary(horse: PreMongoHorse):
             "year": horse.year,
             "country": horse.country,
             "colour": horse.colour,
-            "sire": x["_id"] if horse.sire and (x := get_horse(horse.sire)) else None,
-            "dam": x["_id"] if horse.dam and (x := get_horse(horse.dam)) else None,
+            "sire": get_horse(horse.sire).id if horse.sire else None,
+            "dam": get_horse(horse.dam).id if horse.dam else None,
             "operations": get_operations(horse),
             "ratings": horse.ratings,
         }
